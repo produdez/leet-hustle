fb9458b5811f6a73059cca32fefdbd5d543e43e7
Produde (Long) <72970246+produdez@users.noreply.github.com>
2022-11-07 21:11:21 +0700

    Time: 122 ms (55.99%), Space: 13.9 MB (59.60%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,27 +1,28 @@
 class Solution:
-    def checkFirstLast(self, x):
-        magnitudeOrder = int(math.log10(x))
-        last = x % 10
-        first = x // (10**magnitudeOrder)
-        
-        if last != first: return None
-        mid = (x % 10**magnitudeOrder) // 10
-        if mid == 0: return 0
-        
-        magMid = int(math.log10(mid))
-        if magMid != magnitudeOrder - 2: 
-            # this case is when the mid number has 0-trailings 
-            trailMag = magnitudeOrder - 2 - magMid
-            if mid % (10** trailMag) == 0: 
-                # even trailing 00011000
-                return mid // (10**trailMag)
-            return None # uneven trailing ex 0001011
-        return mid
     def isPalindrome(self, x: int) -> bool:
+        def checkFirstLast(x):
+            # NOTE: mag stands for magnitude order or floor(log10(x))
+            magX = int(math.log10(x))
+            last = x % 10
+            first = x // (10**magX)
+
+            if last != first: return None
+            
+            mid = (x % 10**magX) // 10
+            if mid == 0: return 0
+
+            magMid = int(math.log10(mid))
+            if magMid != magX - 2: # this case is when the mid number has 0-trailings 
+                n_trail = magX - 2 - magMid
+                # even trailing 00011000
+                if mid % (10** n_trail) == 0: return mid // (10**n_trail)
+                
+                return None # uneven trailing ex 0001011
+            return mid
+
         if x==0: return True
         if x<0: return False # fuck this stupid case :)
         while True:
-            print('x: ', x)
-            x = self.checkFirstLast(x)
+            x = checkFirstLast(x)
             if x is None: return False
             if x is 0: return True
