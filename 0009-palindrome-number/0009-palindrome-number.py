d95f2f1f73ec3944ecd7d0943cfb2ccd607a1a22
Produde (Long) <72970246+produdez@users.noreply.github.com>
2022-11-18 20:53:57 +0700

    Time: 187 ms (7.31%), Space: 13.9 MB (16.50%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,25 +1,14 @@
 class Solution:
-    ''' 
-        Update Idea 1
-        
-        still left right check 
-        but now more elegant and not care about trailing zero in mid section anymore
-    '''
     def isPalindrome(self, x: int) -> bool:
-        if x < 0: return False
         if x == 0: return True
+        if x < 0: return False
         
-        magnitude = 10 ** int(math.log10(x))
-        mid = x
-        while mid:
-            print(mid)
-            left = mid // magnitude
-            right = mid % 10
-            if left != right: return False
-
-            mid = (mid % magnitude) // 10
-            magnitude = magnitude // 100
-            # elegance here where your mid could be
-            # number like 0002101 with magnitude 100000 
-            # which makes the trailing 0 still valid 
+        magnitude = math.floor(math.log10(x))
+        while magnitude > 0: 
+            head = x // (10**magnitude)
+            tail = x % 10
+            if head != tail: return False
+            
+            x = x % (10**magnitude) // 10
+            magnitude -= 2
         return True
