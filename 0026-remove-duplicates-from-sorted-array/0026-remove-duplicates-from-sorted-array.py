9944bc24e3d36acf83436609ab2bc96c07750f66
Produde (Long) <72970246+produdez@users.noreply.github.com>
2022-12-15 10:36:09 +0700

    Time: 81 ms (98.97%), Space: 15.7 MB (20.65%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,5 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        '''
+            Solution:
+            Read forward keeping a count of unique numbers
+            If unq_count - 1 < current_index means there's a gap (duplication)
+            so just overwrite the last duplicated index with the current new value
+        '''
         unq_count = 1
         for i in range(1, len(nums)):
             if nums[i] != nums[unq_count - 1]:
