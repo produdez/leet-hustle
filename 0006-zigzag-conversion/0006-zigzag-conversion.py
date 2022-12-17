c4aee3e9627abd9d0c0f0b8669a7815d401857ed
Produde (Long) <72970246+produdez@users.noreply.github.com>
2022-12-17 13:59:33 +0700

    Time: 61 ms (90.76%), Space: 13.9 MB (77.08%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,6 +2,7 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         '''
             Solution
+            Final note: string implementation is faster than list :3
             
             - Idea:
                 Have a container that represents the zig-zag patterns (rows)
@@ -14,14 +15,14 @@ class Solution:
             - Space complexity: mainly the container
                 O(n) or O(numRows * M) with M being the average size of each rows
         '''
-        container = [[] for _ in range(numRows)]
+        container = ['' for _ in range(numRows)]
         
         counter = 0
         increment = -1 if numRows > 1 else 0
         for char in s:
-            container[counter].append(char)
+            container[counter] += char
             
             if counter == 0 or counter == numRows - 1: increment = -increment
             counter = counter + increment
         
-        return ''.join([''.join(arr) for arr in container])
+        return ''.join(container)
