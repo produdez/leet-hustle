f4900f7b63abd334688b6b7fe4e3b8962131e0da
Produde (Long) <72970246+produdez@users.noreply.github.com>
2022-12-17 13:57:47 +0700

    Time: 59 ms (92.58%), Space: 14 MB (77.08%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,13 +1,27 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        container = [[] for _ in range(numRows)]
+        '''
+            Solution
+            
+            - Idea:
+                Have a container that represents the zig-zag patterns (rows)
+                And a counter that changes zig-zagly
+                Keep appending to the correct row and collect results at the end
+            - Time complexity:
+                1. Loop the string O(n)
+                2. Loop and join the rows O(n)
+                => total O(2n) = O(n)
+            - Space complexity: mainly the container
+                O(n) or O(numRows * M) with M being the average size of each rows
+        '''
+        container = ['' for _ in range(numRows)]
         
         counter = 0
         increment = -1 if numRows > 1 else 0
         for char in s:
-            container[counter].append(char)
+            container[counter] += char
             
             if counter == 0 or counter == numRows - 1: increment = -increment
             counter = counter + increment
         
-        return ''.join([''.join(arr) for arr in container])
+        return ''.join(container)
