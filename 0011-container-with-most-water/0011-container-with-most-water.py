c78f267216fa4dd02084fbdd70626bde20ecd82e
Produde (Long) <72970246+produdez@users.noreply.github.com>
2022-12-18 11:05:53 +0700

    Time: 1507 ms (60.22%), Space: 27.4 MB (77.62%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,14 +1,26 @@
 class Solution:
     def maxArea(self, heightList: List[int]) -> int:
+        '''
+        Intuition:
+            1. Start from outside in (meaning reducing the width slowly)
+            2. Given a left_bar and a right_bar (a lower, a larger),
+                - since width is max (we go outside in)
+                - current area <= area(larger, next_inner_height)
+                - meaning we skip checking the smaller bar
+                And if lower = larger, we skip both current left and right
+                    since no way there's a combination of either of them with an inner one that's bigger.
+        '''
         leftIdx = 0
         rightIdx = len(heightList) - 1
         
         largestArea = 0
-        while rightIdx - leftIdx > 0:
+        while True:
+            width = rightIdx - leftIdx
+            if width <= 0: break
 
             leftH, rightH = heightList[leftIdx], heightList[rightIdx]
             height = min(leftH, rightH)
-            largestArea = max((rightIdx - leftIdx) * height, largestArea)
+            largestArea = max(width * height, largestArea)
             
             if height == leftH: leftIdx += 1
             if height == rightH: rightIdx -= 1
