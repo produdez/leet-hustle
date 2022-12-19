996569e0e16061d9105ac7a36ee7879051e0c9d8
Produde (Long) <72970246+produdez@users.noreply.github.com>
2022-12-19 15:38:04 +0700

    Time: 57 ms (84.98%), Space: 13.9 MB (35.15%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,39 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman_dict = {
+            1 : 'I',
+            4 : 'IV',
+            5 : 'V',
+            9 : 'IX',
+            10: 'X',
+            40: 'XL',
+            50: 'L',
+            90: 'XC',
+            100: 'C',
+            400: 'CD',
+            500: 'D',
+            900: 'CM',
+            1000: 'M',
+        }
+        
+        roman_str = ''
+        # magnitude = 10 ** math.floor(math.log10(num))
+        # while magnitude > 0:
+        for magnitude in [10**i for i in range(math.floor(math.log10(num)), -1, -1)]:
+            head_val = (num // magnitude)
+            if head_val == 0: continue
+
+            if magnitude * head_val in roman_dict: 
+                roman_str += roman_dict[magnitude * head_val]
+            else:
+                if head_val > 5:
+                    roman_str += roman_dict[5 * magnitude] 
+                    head_val -= 5
+                
+                roman_str += roman_dict[magnitude] * head_val
+            
+            num = num % magnitude
+        
+        return roman_str
+            
+            
