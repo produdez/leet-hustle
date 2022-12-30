829afcbe46ba20776ab92fc4a162ecf85062dc74
Produde (Long) <72970246+produdez@users.noreply.github.com>
2022-12-30 22:34:11 +0700

    Time: 692 ms (79.70%), Space: 19.4 MB (5.08%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -1,17 +1,37 @@
 from pprint import pprint
 class Solution:
     def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
-        seat_rep = {}
-        for r,c in reservedSeats:
-            if c == 1 or c == 10: continue
-            if not(r in seat_rep): seat_rep[r] = 0b111
+# invalid_counter = {i+1 :set() for i in range(n)}
+#         invalid_counter = collections.defaultdict(set)
+#         for row,col in reservedSeats:
+#             if col == 1 or col == 10: continue
+#             if col in [2,3,4,5]: invalid_counter[row].add(0)
+#             if col in [6,7,8,9]: invalid_counter[row].add(1)
+#             if col in [4,5,6,7]: invalid_counter[row].add(2)
+        
+#         result = 2 * n # max valid
+#         for i in invalid_counter:
+#             count = len(invalid_counter[i])
+#             if count == 3: result -= 2
+#             elif count > 0: result -= 1
+                
+#         return result
 
-            if c in [2,3,4,5]: seat_rep[r] &= ~(1 << 2)
-            if c in [4,5,6,7]: seat_rep[r] &= ~(1 << 1)
-            if c in [6,7,8,9]: seat_rep[r] &= ~(1 << 0)
+        seats = collections.defaultdict(set)
+        # seats = {i+1 :set() for i in range(n)}
+        
+        for i,j in reservedSeats:
+            if j in [2,3,4,5]:
+                seats[i].add(0)
+            if j in [4,5,6,7]:
+                seats[i].add(1)
+            if j in [6,7,8,9]:
+                seats[i].add(2)
+        res = 2*n
+        for i in seats:
+            if len(seats[i]) == 3:
+                res -= 2
+            elif len(seats[i]) > 0:
+                res -= 1
 
-        result = n*2
-        for rep in seat_rep.values():
-            if rep == 0b0: result -= 2
-            else: result -= 1
-        return result
+        return res
