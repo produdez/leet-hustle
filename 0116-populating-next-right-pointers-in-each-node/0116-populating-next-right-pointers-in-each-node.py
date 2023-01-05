58ddaab9f5ada2f27b2f899328d231d63fbe2828
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-05 20:16:03 +0700

    Time: 178 ms (8.04%), Space: 15.5 MB (77.24%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        '''
+            Idea:
+            At every node, you have two pointers
+                1. A go right pointer on your left child (always move right)
+                2. A go left pointer on your right child (always move left)
+            These two pointer will ilterate together and connect:
+                - Left child and right child of current node
+                - All the outer right-most node of the left tree with all the outer left-most node of the right tree
+        '''
+        if not root: return root
+
+        left = root.left
+        right = root.right
+        while left and right:
+            left.next = right
+            left = left.right
+            right = right.left
+        self.connect(root.left)
+        self.connect(root.right)
+        return root
