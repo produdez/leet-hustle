2cb944f3d575436ad8c01ec0a845aa1a5bece8a9
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-06 20:11:31 +0700

    Time: 106 ms (81.55%), Space: 18.5 MB (42.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,19 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         '''
-            Version 2
-                Update with array encode
-            Idea: 
-                Encode all string to a dictionary count 
-                and hash them in order to arrange into groups
-            Complexity:
-                Time: O(#num_str) * O(#num_char)
-                Space: O(#num_str) [since could have unique encodings] * O(#avg_m) [average unique characters]
+            Version 3
+            group by sorting
+            
+            NOTE: This version complexity is O(n * mlog(m)) 
+                m being avg str length
+                n is #str
+            ** This is best performer in this site's result since m is very small (<100)**
         '''
         groups = defaultdict(list)
         for s in strs:
-            count = [0] * 26
-            for char in s:
-                count[ord(char) - 97] += 1
-            groups[tuple(count)].append(s)
+            groups[tuple(''.join(sorted(s)))].append(s)
         return groups.values()
