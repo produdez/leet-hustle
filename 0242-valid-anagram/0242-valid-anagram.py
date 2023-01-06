086b2000a7559688a5f259753201046172ea0e6f
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-06 12:58:02 +0700

    Time: 41 ms (95.81%), Space: 14.4 MB (66.10%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,25 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         '''
-            Version: 2
-            
-            Idea:
-                Build two dict and compare them
-                
-            Complexity:
-                Compared with ver1, better time worse space
-                But still same O(complexity)
-            Time: 
-                O(n) + O(1) since dict compare is O(1)
-            Space:
-                O(2n)
+            Version: 3
+            Cheat using counter
         '''
         if len(t) != len(s): return False
-        
-        ds, dt = {}, {}
-        for i in range(len(s)):
-            ds[s[i]] = ds.get(s[i], 0) + 1
-            dt[t[i]] = dt.get(t[i], 0) + 1
-            
-        return ds == dt
+        return Counter(t) == Counter(s)
         
