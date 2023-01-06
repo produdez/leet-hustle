7811a846c41fd2b173f88abf37b29dcc2fdc8d16
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-06 11:13:29 +0700

    Time: 1130 ms (32.72%), Space: 26.1 MB (28.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,5 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        '''
+            Idea: Set (dictionary hashing)
+            Time: O(n), Space: O(n)
+        '''
         duplicate_set = set()
         
         for num in nums:
