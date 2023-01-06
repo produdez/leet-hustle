df6c5a62b04967d6d9b8e65091172a137911fb5e
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-06 12:44:52 +0700

    Time: 132 ms (18.32%), Space: 14.6 MB (34.47%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        '''
+            Idea:
+            
+            Have a matching dicitonary that maintain the count 
+            of every character
+            - char from s -> +1
+            - char from t -> -1
+            So after adding all chars, all dictionary's entry must be 0
+            
+            Complexity:
+            
+            Time: 
+                O(n) (loop) + O(m) (check sum) -> O(n)
+                with n being #char m being #unique_chars
+            Space:
+                O(m) for the dict
+        '''
+        if len(t) != len(s): return False
+        
+        match = {}
+        for i in range(len(s)):
+            char_s, char_t = s[i], t[i]
+            if char_s in match:
+                match[char_s] += 1
+            else:
+                match[char_s] = 1
+            
+            if char_t in match:
+                match[char_t] -= 1
+            else:
+                match[char_t] = -1
+        
+        for val in match.values():
+            if val != 0: return False
+        return True
+        
