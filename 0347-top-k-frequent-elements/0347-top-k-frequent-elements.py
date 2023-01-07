12eb5e24f1a36f5b05d2c893573117117a078619
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-07 10:06:30 +0700

    Time: 91 ms (99.64%), Space: 18.6 MB (70.32%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,5 +1,9 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        '''
+            Count into a HashMap
+            and sort the keys by value and return k elements
+        '''
         counter = {}
         for num in nums:
             counter[num] = counter.get(num, 0) + 1
