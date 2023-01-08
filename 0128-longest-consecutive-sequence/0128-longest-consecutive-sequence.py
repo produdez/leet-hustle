ad1e39796b727760662923904720053606655e5f
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-08 22:47:08 +0700

    Time: 323 ms (93.78%), Space: 29.1 MB (29.16%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,5 +1,20 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        '''
+            Version 1: 99% fastest
+            
+            Idea:
+                1. convert our list to a set for easy lookup
+                2. Ilterate while popping our set till empty
+                    - get a value
+                    - keep expanding consecutive (head and tail) until unavailable
+                    - Note to remove checked value out of set
+                    - And update longest streak
+            Complexity:
+            - Time: O(n) - convert + O(n) to ilterate and pop all values to check streak
+                -> O(n)
+            - Space: O(n) to store the HashSet that we converted our list into
+        '''
         if not nums: return 0
         nums = set(nums)
         
