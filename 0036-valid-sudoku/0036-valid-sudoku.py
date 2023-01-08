c2da693e550060e071f61840ebd0d280bcf520e1
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-08 13:02:39 +0700

    Time: 103 ms (79.29%), Space: 14 MB (33.62%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,5 +1,14 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        '''
+            Version 1:
+                Naive check all rows/cols/square using HashSet
+                Complexity: Given n as board size (fixed 9 in this case)
+                - Time: O(n^2) (cus checking dup and adding dup takes O(1)
+                - Space: O(n *(3n)) -> O(n^2)
+                    3 sets of size n for each (row/col/square)
+                    -> n * (3n)
+        '''
         def hasDuplicate(val, dup):
             if val == '.': return False
             if val in dup: return True
@@ -11,6 +20,8 @@ class Solution:
             for j in range(9):
                 if hasDuplicate(board[i][j], dup_row): return False
                 if hasDuplicate(board[j][i], dup_col): return False
-                if hasDuplicate(board[(i//3)*3 + j//3][(i%3)*3 + j%3], dup_square): 
-                    return False
+                if hasDuplicate(
+                    board[(i//3)*3 + j//3][(i%3)*3 + j%3], 
+                    dup_square
+                ): return False
         return True
