9c91b84bd40998d38690d56768cecc375cf2723a
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-08 13:31:16 +0700

    Time: 104 ms (77.46%), Space: 13.9 MB (81.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,27 +1,26 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         '''
-            Version 1:
+            Version 1.5: Update simpler code and probably better performance
+                upda
                 Naive check all rows/cols/square using HashSet
                 Complexity: Given n as board size (fixed 9 in this case)
-                - Time: O(n^2) (cus checking dup and adding dup takes O(1)
-                - Space: O(n *(3n)) -> O(n^2)
-                    3 sets of size n for each (row/col/square)
-                    -> n * (3n)
+                
+                >>
         '''
-        def hasDuplicate(val, dup):
-            if val == '.': return False
-            if val in dup: return True
-            dup.add(val)
-            return False
-
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
         for i in range(9):
-            dup_row, dup_col, dup_square = set(), set(), set()
             for j in range(9):
-                if hasDuplicate(board[i][j], dup_row): return False
-                if hasDuplicate(board[j][i], dup_col): return False
-                if hasDuplicate(
-                    board[(i//3)*3 + j//3][(i%3)*3 + j%3], 
-                    dup_square
-                ): return False
+                val = board[i][j]
+                if val == '.': continue
+
+                if val in rows[i]: return False
+                if val in cols[j]: return False
+                if val in squares[(i//3, j//3)]: return False
+                
+                rows[i].add(val)
+                cols[j].add(val)
+                squares[(i//3, j//3)].add(val)
         return True
