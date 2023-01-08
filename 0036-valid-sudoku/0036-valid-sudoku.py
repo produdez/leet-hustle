80eca507226eb57128dd36cf8c13cef94fc1e18d
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-08 13:34:28 +0700

    Time: 96 ms (91.96%), Space: 13.9 MB (33.62%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,12 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         '''
-            Version 1.5: Update simpler code and probably better performance
-                upda
-                Naive check all rows/cols/square using HashSet
-                Complexity: Given n as board size (fixed 9 in this case)
-                
-                >>
+            Version 1.5: Update simpler code 
+                and probably better performance update
+            Update:
+                Difference with previous version is that we put current cell into
+                their correct set
+                Instead of finding the approperiate cell for each set in order
+            Idea:
+            Naive check all rows/cols/square using HashSet
+
+            Complexity: Given n as board size (fixed 9 in this case)
+            - Time: O(n^2): ilterate all cells
+            - Space: O(3* n^2): dictionaries of hash set to check dup
+                - all cells n^2 * 3 for row/col/square
         '''
         rows = defaultdict(set)
         cols = defaultdict(set)
