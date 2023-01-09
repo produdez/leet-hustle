3be67b654ce63d0075af6b9539f6aedee8825318
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-09 18:36:21 +0700

    Time: 65 ms (90.02%), Space: 14.4 MB (55.47%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,6 +1,8 @@
 class Solution:
     '''
-        Version: 1
+        Version: 2
+            Simpler code, eliminate wasted if checks
+            + Push the converted value in so that we dont need to convert when pop
         Idea: Push pending values to a stack, pop out when see an operator
             Calculate and push back into stack
         Complexity:
@@ -10,16 +12,16 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         wait = []
         for token in tokens:
-            if token not in ['+', '-', '*', '/']:
-                wait.append(token)
-                continue
-            
-            right = int(wait.pop())
-            left = int(wait.pop())
             match token:
-                case '+': wait.append(left + right)
-                case '-': wait.append(left - right)
-                case '*': wait.append(left * right)
-                case '/': wait.append(left/right)
+                case '+': wait.append(wait.pop() + wait.pop())
+                case '-': 
+                    r, l = wait.pop(), wait.pop()
+                    wait.append(l - r)
+                case '*': wait.append(wait.pop() * wait.pop())
+                case '/': 
+                    r, l = wait.pop(), wait.pop()
+                    wait.append(int(l/r))
+                case _:
+                    wait.append(int(token))
         
         return int(wait[0])
