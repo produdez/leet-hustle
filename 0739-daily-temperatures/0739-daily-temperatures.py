a05ff88aa537862cd306c9610dfecdbe1fc15980
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-10 21:13:31 +0700

    Time: 1936 ms (58.19%), Space: 29.7 MB (17.58%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,16 +1,30 @@
 import heapq
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        result = [0] * len(temperatures)
-        heap = []
-        for i, num in enumerate(temperatures):
-            while heap and heap[0][0] < num:
-                head = heapq.heappop(heap)
-                result[head[1]] = i - head[1]
+        '''
+            Version 1
             
-            heapq.heappush(heap, (num, i))
+            Idea:
+                Keep a sorted MIN heap of temperature and their respective index
+                Ilterate the temperature array and
+                    - When current temp > heap head temp (meaning has results)
+                    - We keep poping the heap and get the index difference
+                    - Then we put the current (temp, index) into the heap
+            Complexity:
+            - Time: O(n*(log(n)))
+            - Space: O(n) result array + O(n) heap 
+                -> O(n) 
+        '''
+        result = [0] * len(temperatures)
+        heap = [] # heap of tuple (temperature, index)
+        for index, temp in enumerate(temperatures):
+            while heap and heap[0][0] < temp:
+                head = heapq.heappop(heap)
+                result[head[1]] = index - head[1]
+            
+            heapq.heappush(heap, (temp, index))
         
-        for num, i in heap:
+        for _, i in heap:
             result[i] = 0
         return result
             
