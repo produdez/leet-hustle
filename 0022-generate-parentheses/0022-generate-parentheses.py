8942909044610842c2f9a82f507d250f148e2e15
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-10 18:07:22 +0700

    Time: 45 ms (60.59%), Space: 14.2 MB (38.61%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,39 +1,32 @@
-class ParenRep:
-    def __init__(self, rep, unclosed, rl, rr):
-        self.rep = rep
-        self.unclosed = unclosed
-        self.rl = rl
-        self.rr = rr
-    def isOpen(self):
-        return self.unclosed > 0
-    def addOpen(self):
-        return ParenRep(self.rep + ['('], self.unclosed+1, self.rl-1, self.rr)
-    def addClose(self):
-        return ParenRep(self.rep + [')'], self.unclosed-1, self.rl, self.rr-1)
-    def finished(self):
-        return self.rl == 0 and self.rr == 0
-
-    def __str__(self): return ''.join(self.rep)
-    
 class Solution:
-    '''
-        Version 1
-        Idea:
-            Generate all possible cases
-            Using stack to go DFS (BFS is also okay with queue)
-        Complexity:
-            Time = Space = LoopCount = #Possible results
-    '''
     def generateParenthesis(self, n: int) -> List[str]:
-        stack = [ParenRep(['('], 1, n-1, n)]
-        result = []
-        while stack:
-            curr = stack.pop()
-            if curr.finished():
-                result.append(str(curr))
-                continue
-
-            if curr.rl > 0: stack.append(curr.addOpen())
-            if curr.isOpen(): stack.append(curr.addClose())
+        '''
+            Version 2
             
+            Idea:
+                Same idea, gen all cases
+                But now uses backtracking approach (DFS)
+                and possibly less space
+            Complexity:
+                = Tree size
+        '''
+        
+        stack = [] # this stack keeps track of the current result we're building
+        result = []
+        def backtrack(l, r):
+            if l == r == n: 
+                result.append(''.join(stack))
+
+            if l >= r and l < n:
+                stack.append('(')
+                backtrack(l+1, r)
+                stack.pop()
+            
+            if r < n:
+                stack.append(')')
+                backtrack(l, r+1)
+                stack.pop()
+        
+        backtrack(0,0)
         return result
+                
