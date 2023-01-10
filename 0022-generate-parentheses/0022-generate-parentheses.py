0cca97c3d9766a9a966958a7386ae5a21e567032
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-10 17:00:55 +0700

    Time: 41 ms (69.35%), Space: 14.2 MB (75.94%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -7,18 +7,17 @@ class ParenRep:
     def isOpen(self):
         return self.unclosed > 0
     def addOpen(self):
-        return ParenRep(self.rep + '(', self.unclosed+1, self.rl-1, self.rr)
+        return ParenRep(self.rep + ['('], self.unclosed+1, self.rl-1, self.rr)
     def addClose(self):
-        return ParenRep(self.rep + ')', self.unclosed-1, self.rl, self.rr-1)
+        return ParenRep(self.rep + [')'], self.unclosed-1, self.rl, self.rr-1)
     def finished(self):
         return self.rl == 0 and self.rr == 0
-    def __str__(self):
-        return str({
-            'rep': self.rep, 'rleft':self.rl, 'rright':self.rr, 'open':self.unclosed
-        })
+
+    def __str__(self): return ''.join(self.rep)
+    
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        stack = [ParenRep('(', 1, n-1, n)]
+        stack = [ParenRep(['('], 1, n-1, n)]
         result = []
         while stack:
             curr = stack.pop()
@@ -29,5 +28,5 @@ class Solution:
                 if curr.rr > 0: stack.append(curr.addClose())
             
             if curr.finished():
-                result.append(curr.rep)
+                result.append(str(curr))
         return result
