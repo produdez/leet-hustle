e3ba8a5e30cb3e86425897fd2d16f2c02769efd5
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-11 11:39:09 +0700

    Time: 1161 ms (53.32%), Space: 36.9 MB (24.42%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,20 +1,14 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars = sorted(zip(position, speed), key=lambda x: x[0])
-        def catchable(car1, car2):
-            x1,v1 = car1
-            x2, v2 = car2
-            if x1==x2: return True
+        def catchable(x1,v1, x2, v2):
             if (x1-x2)*(v1-v2) >= 0: return False
-            meet = (x1-x2)/(v2-v1) * v1 + x1
-            if meet > target: return False
-            return True
-        
-        stack = [cars[0]]
-        for car in cars[1:]:
-            while stack and catchable(car, stack[-1]):
-                car2 = stack.pop()
-                car = car if car[1] < car2[1] else car2
+            return (x1-x2)/(v2-v1) * v1 + x1 <= target
+
+        cars = sorted(zip(position, speed), key=lambda x: x[0])        
+        stack = []
+        for car in cars:
+            while stack and catchable(*car, *stack[-1]):
+                stack.pop()
             stack.append(car)
         return len(stack)
             
