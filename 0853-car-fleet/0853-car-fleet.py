efe2abe01303507da4b8f0841a27b4f6a5708019
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-11 11:46:04 +0700

    Time: 1145 ms (54.41%), Space: 37.6 MB (15.42%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,16 +1,36 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        '''
+            Version 1
+            
+            Idea:
+                1. Sort cars by position so that we can reduce the cars/result
+                2. Keep adding car into fleet
+                    - If reducible, reduce the fleet into the current car (furthest slowest car)
+                    - If not reducible, just add as a new fleet
+                    - Note that when a car can reduce a fleet, keep checking
+            Complexity:
+            - Time: O(nlogn)-sort + O(2n)-reduce fleet
+            - Space: O(1) or O(n) for sorting + O(n) for fleet result
+            
+            Note: why O(2n) to reduce fleet
+            - Cause we ilterate n times 
+            - And everytime we can reduce we reduce
+            - #reduce = #car
+            -> so total reduce time is n
+            -> 2n
+        '''
         def catchable(x1,v1, x2, v2):
             if (x1-x2)*(v1-v2) >= 0: return False
             return (x1-x2)/(v2-v1) * v1 + x1 <= target
 
         cars = sorted(zip(position, speed), key=lambda x: x[0])        
-        stack = []
+        fleets = []
         for car in cars:
-            while stack and catchable(*car, *stack[-1]):
-                stack.pop()
-            stack.append(car)
-        return len(stack)
+            while fleets and catchable(*car, *fleets[-1]):
+                fleets.pop()
+            fleets.append(car)
+        return len(fleets)
             
             
                 
