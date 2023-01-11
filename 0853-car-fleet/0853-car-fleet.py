0387ec3d63b7028ec21548d15dbdc12e588647d8
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-11 12:02:49 +0700

    Time: 947 ms (86.92%), Space: 37.2 MB (19.98%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,34 +1,25 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         '''
-            Version 1
+            Version 2: Update simplicity + intuition
             
             Idea:
-                1. Sort cars by position so that we can reduce the cars/result
-                2. Keep adding car into fleet
-                    - If reducible, reduce the fleet into the current car (furthest slowest car)
-                    - If not reducible, just add as a new fleet
-                    - Note that when a car can reduce a fleet, keep checking
+                1. sort by position
+                2. GO FROM RIGHT TO LEFT
+                3. If reducible, reduce to the fleet head (the slower right car)
+                4. reducible can be check by compare arrive time to target
             Complexity:
-            - Time: O(nlogn)-sort + O(2n)-reduce fleet
-            - Space: O(1) or O(n) for sorting + O(n) for fleet result
-            
-            Note: why O(2n) to reduce fleet
-            - Cause we ilterate n times 
-            - And everytime we can reduce we reduce
-            - #reduce = #car
-            -> so total reduce time is n
-            -> 2n
+            - Time: O(nlogn)-sort + O(n) fleet build
+            - Space: O(n) for sorted array + O(n) for stack
         '''
-        def catchable(x1,v1, x2, v2):
-            if (x1-x2)*(v1-v2) >= 0: return False
-            return (x1-x2)/(v2-v1) * v1 + x1 <= target
 
-        cars = sorted(zip(position, speed), key=lambda x: x[0])        
+        cars = sorted(zip(position, speed), key=lambda x: x[0], reverse=True)       
         fleets = []
+        catchable = lambda c1, c2: (target-c2[0])/c2[1] >= (target-c1[0])/c1[1]
+        
         for car in cars:
-            while fleets and catchable(*car, *fleets[-1]):
-                fleets.pop()
+            if fleets and catchable(car, fleets[-1]):
+                continue
             fleets.append(car)
         return len(fleets)
             
