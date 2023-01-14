3e007ce6f6549a622f70f270e327857b55898a83
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-14 16:26:21 +0700

    Time: 58 ms (62.38%), Space: 14.6 MB (77.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,10 +3,10 @@ class Solution:
         
         l, r = 0, len(s) - 1
         while l < r:
-            if not s[l].isalpha() and not s[l].isnumeric(): 
+            if not s[l].isalnum(): 
                 l += 1
                 continue
-            if not s[r].isalpha() and not s[r].isnumeric(): 
+            if not s[r].isalnum(): 
                 r -= 1
                 continue
             if s[l].lower() != s[r].lower(): return False
