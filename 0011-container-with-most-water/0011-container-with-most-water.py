777bf4c42b6decc9602dbb8de9faebb154f5bb1d
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-16 17:50:17 +0700

    Time: 718 ms (94.72%), Space: 27.5 MB (11.36%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,31 +1,20 @@
 class Solution:
-    def maxArea(self, heightList: List[int]) -> int:
-        '''
-        Version 1.0
-
-        Intuition:
-            1. Start from outside in (meaning reducing the width slowly)
-            2. Given a left_bar and a right_bar (a lower, a larger),
-                - since width is max (we go outside in)
-                - current area <= area(larger, next_inner_height)
-                - meaning we skip checking the smaller bar
-                And if lower = larger, we skip both current left and right
-                    since no way there's a combination of either of them with an inner one that's bigger.
-        '''
-        leftIdx = 0
-        rightIdx = len(heightList) - 1
-        
-        largestArea = 0
-        while True:
-            width = rightIdx - leftIdx
-            if width <= 0: break
-
-            leftH, rightH = heightList[leftIdx], heightList[rightIdx]
-            height = min(leftH, rightH)
-            largestArea = max(width * height, largestArea)
-            
-            if height == leftH: leftIdx += 1
-            if height == rightH: rightIdx -= 1
-        
-        return largestArea 
-
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        max_area = 0
+        while l < r:
+            # print(l, r, height[l] ,height[r])
+            w = (r-l)
+            if height[l] > height[r]:
+                min_h = height[r]
+                r -= 1
+            elif height[l] < height[r]:
+                min_h = height[l]
+                l += 1
+            else:
+                min_h =  height[l]
+                l += 1
+                r -= 1
+            max_area = max(max_area, w*min_h)
+            # print(min_h, w, max_area)
+        return max_area
