ca8e40703c319d392eaedade6c50200b90ab830e
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-18 13:04:17 +0700

    Time: 142 ms (64.39%), Space: 15.9 MB (78.55%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,22 +1,16 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
+    def trap(self, height: List[int]) -> int:   
+        left, right = 0, len(height) - 1
+        maxLeft, maxRight, water = 0, 0, 0
         
-        left = 0
-        right = len(height) - 1
-        maxLeft = 0
-        maxRight = 0
-        water = 0
-        while left < right - 1: # means there's a gap between
+        while left < right - 1: # means there's a gap between (min size 3)
             maxLeft = max(maxLeft, height[left])
             maxRight = max(maxRight, height[right])
+            
             if maxLeft <= maxRight:
-                smallerMax = maxLeft
-                curr = left + 1
+                water += max(0, maxLeft - height[left + 1])
                 left += 1
             else:
-                smallerMax = maxRight
-                curr = right - 1
-                right -= 1
-            water += max(0, smallerMax - height[curr])
-            
+                water += max(0, maxRight - height[right - 1])
+                right -= 1            
         return water
