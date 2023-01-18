3e0b333faa05d8d7cbf6eccdcb4ef29d317c2288
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-18 13:11:45 +0700

    Time: 134 ms (71.05%), Space: 16.1 MB (42.03%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -7,10 +7,10 @@ class Solution:
         while left < right - 1: # means there's a gap between (min size 3)
             if maxLeft <= maxRight:
                 left += 1
-                water += max(0, maxLeft - height[left])
                 maxLeft = max(maxLeft, height[left])
+                water += maxLeft - height[left]
             else:
                 right -= 1
-                water += max(0, maxRight - height[right])
                 maxRight = max(maxRight, height[right])
+                water += maxRight - height[right]
         return water
