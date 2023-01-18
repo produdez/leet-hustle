b4d3a293423446a1a608a538ae1900a4337907af
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-18 13:01:58 +0700

    Time: 146 ms (61.74%), Space: 16 MB (78.55%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,39 +1,23 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        '''
-            Version 1: Nice intuition but hard to understand
-            Idea:
-            - Keep monotonic-decresing stack ordered by height
-            - Each node keeps track of it's sunken value, which includes
-                - smaller nodes that comes before it
-                - and water trapped that's already calculated
-            - So we keep adding node to the stack if height is larger, else:
-                1. Calculate trapped water (note to subtract sunken of current node and sunken of the last node in stack)
-                2. Update total water and sunken stuffs
-                3. Repeat until the stack is decreasing again
-            - Lastly we go through the stack once more to add the water trapped between
-            the remaining ones
-            
-            NOTE: why decreasing stack ?
-                Cus we can only calculate sunken bottom up
-            Complexity:
-            - Time: O(2n): everything is added once and removed once
-            - Space: O(n)
-        '''
-        stack = [] # (index, height, sunken) , ordered decreasingly by height
-        water = 0
-
-        for i, h in enumerate(height):
-            sunken = 0
-            while stack and h >= stack[-1][1]:
-                prev_i, prev_h, prev_sunken = stack.pop()
-                trapped = (i - prev_i - 1) * prev_h - sunken
-                water += trapped
-                sunken += trapped + prev_h + prev_sunken
-            stack.append((i,h,sunken))
+        if len(height) < 3: return 0
         
-        while len(stack) >= 2:
-            i, h, sunken = stack.pop()
-            water += h * (i - stack[-1][0] - 1) - sunken
+        left = 0
+        right = len(height) - 1
+        maxLeft = 0
+        maxRight = 0
+        water = 0
+        while left < right - 1: # means there's a gap between
+            maxLeft = max(maxLeft, height[left])
+            maxRight = max(maxRight, height[right])
+            if maxLeft <= maxRight:
+                smallerMax = maxLeft
+                curr = left + 1
+                left += 1
+            else:
+                smallerMax = maxRight
+                curr = right - 1
+                right -= 1
+            water += max(0, smallerMax - height[curr])
             
         return water
