44a386fcd51ac3e80a471118f4504f224383d47a
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-18 11:31:07 +0700

    Time: 130 ms (76.03%), Space: 16 MB (78.55%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,20 +1,33 @@
-# class Column:
-    # def __init__(self, index, height, sunken_weight=0):
-    #     self.index = index
-    #     self.height = height
-    #     self.sunken_weight = sunken_weight
-
 class Solution:
     def trap(self, height: List[int]) -> int:
-        stack = [] 
+        '''
+            Version 1:
+            
+            Idea:
+            - Keep monotonic-decresing stack ordered by height
+            - Each node keeps track of it's sunken value, which includes
+                - smaller nodes that comes before it
+                - and water trapped that's already calculated
+            - So we keep adding node to the stack if height is larger, else:
+                1. Calculate trapped water (note to subtract sunken of current node and sunken of the last node in stack)
+                2. Update total water and sunken stuffs
+                3. Repeat until the stack is decreasing again
+            - Lastly we go through the stack once more to add the water trapped between
+            the remaining ones
+            
+            NOTE: why decreasing stack ?
+                Cus we can only calculate sunken bottom up
+        '''
+        stack = [] # (index, height, sunken) , ordered decreasingly by height
         water = 0
+
         for i, h in enumerate(height):
             sunken = 0
-            while stack and stack[-1][1] <= h:
+            while stack and h >= stack[-1][1]:
                 prev_i, prev_h, prev_sunken = stack.pop()
-                increment = (i - prev_i - 1) * prev_h - sunken
-                water += increment
-                sunken += increment + prev_h + prev_sunken
+                trapped = (i - prev_i - 1) * prev_h - sunken
+                water += trapped
+                sunken += trapped + prev_h + prev_sunken
             stack.append((i,h,sunken))
         
         while len(stack) >= 2:
