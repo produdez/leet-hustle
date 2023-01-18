0c8e9577d90c71bfe6486fcd64ae2bc41bbdc21c
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-18 13:20:01 +0700

    Time: 127 ms (80.36%), Space: 15.9 MB (78.55%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,16 +1,44 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:   
+    def trap(self, height: List[int]) -> int:
+        '''
+            Version 2:
+                Optimal: Neetcode 
+            Complexity:
+            - Time: O(n)
+            - Space: O(1)
+            
+            Idea: 
+                Notice that the water that can be trapped at every column is
+                    min(maxLeft, maxRight) - height if > 0 else 0
+                So we keep a left pointer and right pointer to track maxLeft and maxRight
+                At every ilteration, we calculate the watter trap next to the smaller max
+                Meaning:
+                    - if maxLeft < maxRight -> calculate at idx left + 1
+                    - if maxRight < maxLeft -> calculate at idx right - 1
+                Why?
+                    Because when one max is smaller, the equation min(maxLeft, maxRight)
+                    equals that max
+                    -> and that is the value for column at location next to that smaller max
+            Note:
+                There are some tricks in the code
+                1. Increase left/right index instead of calculate current location
+                    curr = left + 1 or right - 1
+                2. By updating the max before updating the water, it is made 
+                    sure that the trapped value is never negative
+                    if 
+                
+        '''
         left, right = 0, len(height) - 1
         maxLeft, maxRight = height[left], height[right]
         water = 0
         
-        while left < right - 1: # means there's a gap between (min size 3)
+        while left < right: # means there's a gap between (min size 3)
             if maxLeft <= maxRight:
-                left += 1
+                left += 1 # curr = left + 1
                 maxLeft = max(maxLeft, height[left])
                 water += maxLeft - height[left]
             else:
-                right -= 1
+                right -= 1 # curr = right - 1
                 maxRight = max(maxRight, height[right])
                 water += maxRight - height[right]
         return water
