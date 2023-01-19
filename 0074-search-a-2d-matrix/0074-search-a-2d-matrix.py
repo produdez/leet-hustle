b6db85c6b24da28bf0ab72c5535c4ac6e1e70094
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-19 18:49:00 +0700

    Time: 43 ms (88.13%), Space: 14.5 MB (6.32%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,9 +3,7 @@ class Solution:
         left , right = 0, len(matrix) * len(matrix[0]) - 1
         while True:
             pv = (left + right) // 2
-            x = pv // len(matrix[0])
-            y = pv % len(matrix[0])
-            value = matrix[x][y]
+            value = matrix[pv // len(matrix[0])][pv % len(matrix[0])]
             if target == value: return True
             
             if left == right == pv: break
