a8be57796d74a63f22076cc9569c15f31c0f8d64
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-22 14:30:55 +0700

    Time: 44 ms (71.10%), Space: 14.1 MB (58.27%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,5 +1,21 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        '''
+            Version 1
+            
+            Idea:
+            1. No matter where you are on the circle, there's bound to be some one smaller than you on your left or right
+            2. Check step very big and then smaller, smaller (div by 2)
+                If you're a very big number, you'll quickly walk towards smaller
+                If you're a small number, you'll wait until the steps are small enough for you to walk around
+            3. At the end, exactly after log2(len) steps, you are bound to be
+            at the minimum or next to it
+                -> that's why result is min(pivot, pivot - 1)
+                
+            Complexity:
+            - Time: O(log(n))
+            - Space: O(1)
+        '''
         pivot = len(nums) // 2
         i = len(nums) // 2
         
