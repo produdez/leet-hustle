4da717f73f8532eccaac52658c45f79de0907764
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-24 16:52:57 +0700

    Time: 782 ms (74.17%), Space: 71.5 MB (70.13%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,5 +1,24 @@
 class TimeMap:
-
+    '''
+        Version: 1
+        Note: 
+            Binary search here
+            1. Wants the max of the smaller value
+            2. So I used the left pointer as the variable
+        Idea:
+            HashMap with 
+            - Key: string
+            - Value: Array of Tuple (timestamp, value)
+            Binary search based on timestamp to get value
+        Complexity:
+        - set:
+            - Time: O(1) unless when array need to be resized
+            - Space: O(1) no extra space
+        - get
+            - Time: O(1) to get the array + O(log(n)) to search
+                for value using timestamp
+            - Space: O(1)
+    '''
     def __init__(self):
         self.dict = collections.defaultdict(list)
 
@@ -11,8 +30,6 @@ class TimeMap:
         if not arr or arr[0][0] > timestamp: return ""
         
         left, right = 0, len(arr) - 1
-        
-        res, ts_res = "", 0
         while left < right:
             pivot = (left + right) // 2 + 1
             if arr[pivot][0] == timestamp: return arr[pivot][1]
