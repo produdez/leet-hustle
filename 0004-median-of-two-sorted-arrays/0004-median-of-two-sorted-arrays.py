0401bbbfab5569c3f4a8b42b6f6d5651d9c9bb1e
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-27 20:27:31 +0700

    Time: 201 ms (26.30%), Space: 14.1 MB (61.63%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,6 +1,35 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        '''
+            Version: 1
+            
+            Idea:
+            Binary search the left-portion of the median by:
+            1. Getting the pivot of the first array (binary search)
+            2. Prepare the left portion of median by getting the rest
+                from the second array
+            -> left por - first array pivot ... = second array pivot
+            3. Validate result
+                - Second array index must be valid (I)
+                - left portion median split must be valid (II)
+                -> first array's left <= second array's right
+                -> and vice versa
+            4. If split is valid
+                -> Get result by
+                if odd -> get mid max of the two right sides
+                if even -> get average of left side's max 
+                and right side's min
+            Techniques:
+            1. Swap the arrays so we always have longer array as nums1
+            2. Add -inf and +inf at the two ends of the arrays
+            to remove edge cases
+            3. (I) invalid pivot 2
+            - If piv2 < 0 -> move piv 1 to the left -> right = piv1 - 1
+            - if piv2 is at last nums2 index -> reverse -> left = piv1 + 1
+            
+        '''
         if len(nums2) > len(nums1): nums1, nums2 = nums2, nums1
+
         for arr in [nums1,nums2]:
             arr.append(math.inf)
             arr.insert(0, -math.inf)
