e4d64dcf8bd917b7de25784e4fec51788be33c1c
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-27 20:06:02 +0700

    Time: 105 ms (51.70%), Space: 14.3 MB (20.47%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -13,24 +13,28 @@ class Solution:
         while left <= right:
             pivot1 = (left + right) // 2
             
-            remain2 = left_portion_size - (pivot1 + 1)
-            print(f'remain2 {remain2}, init p1: {pivot1}')
-            if remain2 < 0:
-                # pivot1 += remain2
-                # pivot2 = 0
-                right = pivot1 - 1
-                continue
-            elif remain2 >= len(nums2):
-                # pivot1 += (remain2 - len(nums2)) + 1
-                # pivot2 = len(nums2) - 2
-                left = pivot1 + 1
-                continue
-            # else:
-                # pivot2 = remain2 - 1
-            pivot2 = remain2 - 1
+            pivot2 = left_portion_size - (pivot1 + 1) - 1
+            # print(f'remain2 {remain2}, init p1: {pivot1}')
+            # if remain2 < 0:
+            #     # pivot1 += remain2
+            #     # pivot2 = 0
+            #     right = pivot1 - 1
+            #     continue
+            # elif remain2 >= len(nums2):
+            #     # pivot1 += (remain2 - len(nums2)) + 1
+            #     # pivot2 = len(nums2) - 2
+            #     left = pivot1 + 1
+            #     continue
+            # # else:
+            #     # pivot2 = remain2 - 1
+            # pivot2 = remain2 - 1
             print(f'p1: {pivot1}, p2: {pivot2}')
             
-            if nums1[pivot1] > nums2[pivot2 + 1]: 
+            if pivot2 <= -1:
+                right = pivot1 - 1
+            elif pivot2 >= len(nums2) - 1:
+                left = pivot1 + 1
+            elif nums1[pivot1] > nums2[pivot2 + 1]: 
                 # means nums1's pivot is too large
                 right = pivot1 - 1
             elif nums2[pivot2] > nums1[pivot1 + 1]:
