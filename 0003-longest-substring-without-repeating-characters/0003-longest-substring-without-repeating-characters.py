6e8731f7e39bfac2b65b9482e1b9dc38eaeedcda
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-28 23:12:15 +0700

    Time: 155 ms (31.78%), Space: 14.1 MB (47.03%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,13 +1,32 @@
 class Solution:
+    '''
+        Version: 1
+        
+        Idea: Two pointers
+        1. left/right points at sides of the non-duplicated string
+        2. Loop through and
+            - Check duplicate
+            - Add new char to the unique dictionary
+        3. When is duplicate !
+            - Get + update maxLength
+            - Remove all chars from 
+                - Current duplicated index
+                - To the current left
+                From the unique dictionary
+            - Move left pointer to right after the duplicated index
+            - Override the duplicated index with the new index
+    '''
     def lengthOfLongestSubstring(self, s: str) -> int:
         left = 0
-        uniq = {}
+        uniq = {} # store unique char and it's index
         maxLen = 0
         for right, char in enumerate(s):
             if char in uniq:
-                maxLen = max(maxLen, right - left)
+                maxLen = max(maxLen, right - left) 
                 for c in s[left: uniq[char]]:
                     uniq.pop(c)
                 left = uniq[char] + 1
-            uniq[char] = right
+            
+            # add new char or override index of duplicated char
+            uniq[char] = right 
         return max(maxLen, len(s) - left)
