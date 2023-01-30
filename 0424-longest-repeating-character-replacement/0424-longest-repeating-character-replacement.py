a062644b38f90b431278fefb48d2d04aebd042da
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-30 11:38:48 +0700

    Time: 171 ms (58.95%), Space: 14 MB (15.96%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -2,10 +2,23 @@ class Solution:
     '''
         Version: 2
         
-        Idea:
-        
+        Idea: Simple expanding window
+            A window [left, right] represent a sub string
+            Valid if len(window) - frequency(most_occur_char) <= k
+            1. So when invalid
+                - Strink window from the left 
+                - And update occurance/maxFreq
+            2. Till it's valid
+                - Update max len
+            3. Also note to update occur/max Freq when expanding window
+
         Complexity:
-        Time: O(n * m) where m is #unique chars (this case 26)
+        Time: O(2 * n * m) where m is #unique chars (this case 26)
+            Every char is added in once
+            Every char could be popped out once
+            -> 2n
+            Each time add/pop -> update takes m 
+            -> 2n * m
         Space: O(n) for the occurence dictionary
     '''
     def characterReplacement(self, s: str, k: int) -> int:
@@ -15,9 +28,8 @@ class Solution:
         for right, rChar in enumerate(s):
             occur[rChar] += 1
             maxFreq = max(occur.values()) # O(m=26)
-            # print('.....')
+            
             while right - left + 1 - maxFreq > k: #invalid
-                # print(occur)
                 occur[s[left]] -= 1
                 left += 1
                 maxFreq = max(occur.values())
