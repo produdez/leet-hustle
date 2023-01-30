66c1f3583b7f617b3ddf5ec26bb72c03175e6194
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-01-30 11:31:50 +0700

    Time: 184 ms (50.66%), Space: 14 MB (53.71%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,23 +1,28 @@
 class Solution:
+    '''
+        Version: 2
+        
+        Idea:
+        
+        Complexity:
+        Time: O(n * m) where m is #unique chars (this case 26)
+        Space: O(n) for the occurence dictionary
+    '''
     def characterReplacement(self, s: str, k: int) -> int:
-        '''
-            Version: 1.evil
-            
-            Idea: Will come back to later
-        '''
-        occur = collections.defaultdict(int) # 0
         left = 0
-        mostFreq = ''
+        occur = collections.defaultdict(int) # 0
         maxLen = 0
-        for right, char in enumerate(s):
-            occur[char] += 1
-            if occur[char] > occur[mostFreq]:
-                mostFreq = char
-            
-            strLen = (right - left + 1)
-            if strLen - occur[mostFreq] <= k:
-                maxLen = max(maxLen, strLen)
-            else:
+        for right, rChar in enumerate(s):
+            occur[rChar] += 1
+            maxFreq = max(occur.values()) # O(m=26)
+            # print('.....')
+            while right - left + 1 - maxFreq > k: #invalid
+                # print(occur)
                 occur[s[left]] -= 1
                 left += 1
+                maxFreq = max(occur.values())
+            
+            maxLen = max(maxLen, right - left + 1)
+        
         return maxLen
+                
