79ed9aa3fe1977bc3acd7e5981e88a725e7ea025
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-02-03 15:01:09 +0700

    Time: 56 ms (97.03%), Space: 13.9 MB (64.82%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,29 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s2) < len(s1): return False
-        left, right = 0, len(s1)
+        left, right = 0, len(s1) - 1
         
-        dict_s1 = collections.defaultdict(int)
+        dict_s1 = {}
         for c in s1:
-            dict_s1[c] += 1
+            dict_s1[c] = dict_s1.get(c, 0) + 1
+        dict_window = {}
+        for c in s2[left: right + 1]:
+            dict_window[c] = dict_window.get(c, 0) + 1
         
-        while right < len(s2) + 1:
-            if s2[left] in dict_s1:
-                temp_left = left
-                while temp_left < right:
-                    if dict_s1[s2[temp_left]] == 0:
-                        temp_left -= 1
-                        while temp_left >= left:
-                            dict_s1[s2[temp_left]] += 1
-                            temp_left -= 1
-                        break
-                    
-                    dict_s1[s2[temp_left]] -= 1
-                    temp_left += 1
-                if temp_left == right: return True
+        while True:
+            # print('window: ', s2[left: right + 1])
+            # print('dw: ', dict_window)
+            if dict_window == dict_s1: return True
+            
+            if dict_window[s2[left]] == 1: del dict_window[s2[left]]
+            else: dict_window[s2[left]] -= 1
             left += 1
             right += 1
-        return False
+            if right >= len(s2): return False
+            dict_window[s2[right]] = dict_window.get(s2[right], 0) + 1
                 
                     
             
