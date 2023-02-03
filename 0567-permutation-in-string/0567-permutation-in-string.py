07c1e1517b0c7eac1eda71053d2a55093ea0c64e
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-02-03 14:29:43 +0700

    Time: 2454 ms (20.97%), Space: 14 MB (64.82%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,6 +1,23 @@
 class Solution:
+    '''
+        Version: 1
+        
+        Idea: 
+        1. Have a window of size s1 slide through s2
+            And check if window matches s1's permutation
+        2. Check match using hash map of char occurance
+        
+        Complexity:
+        - Time: O(n2 - n1) - window slide time
+            * O(n1) - time to check HashMap match
+            n1: len(s1), n2: len(s2)
+            -> Total O(m * n) where m is str diff and n is len of window
+        - Space: O(2 * n1)
+            At all time we just need hash map of s1 and hash map of window
+    '''
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s2) < len(s1): return False
+
         window = [0, len(s1)]
         dict_s1 = collections.defaultdict(int)
         for c in s1:
