b007d4ea4990c6b5280b50d94427af3a4a7fcf73
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-02-11 10:31:19 +0700

    Time: 2186 ms (31.72%), Space: 30.2 MB (34.75%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        stack = [nums[0]]
+        for num in nums[1: k]:
+            while stack and num > stack[-1]: stack.pop()
+            stack.append(num)
+        
+        left = 0
+        result = [stack[0]]
+        for right in range(k, len(nums)):
+            # print('new: ', nums[right])
+            # print('stack: ', stack)
+            if nums[left] == stack[0]: stack.pop(0)
+            left += 1
+            
+            while stack and nums[right] > stack[-1]: 
+                stack.pop()
+            stack.append(nums[right])
+            result.append(stack[0])
+        return result
