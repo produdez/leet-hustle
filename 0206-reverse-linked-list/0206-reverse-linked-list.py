806ae05054ffa6cd8d883c2f5388ab7245da46f4
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-02-15 19:55:31 +0700

    Time: 43 ms (39.85%), Space: 20.4 MB (13.98%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -6,7 +6,19 @@
 class Solution:
     '''
         Version: 2.5
-            
+            Advanced recursive
+        Idea:
+            Imagine our list 1 -> 2 -> 3  
+            If we reverse after 1 
+            we have: 1 -> 2 <- 3 (2 points to None also)
+            That's the idea:
+            1. We reverse after current node
+            2. Current node always points to tail of reverse list
+                so we use that curr.next.next to point reverse tail
+                to current node
+            3. Always set the current node.next to None
+                to make sure the reversed list is terminated and no
+                loop
     '''
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next: return head
