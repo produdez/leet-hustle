219ab84e62b6d11ddeac062e73ffd870e48a1e06
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-02-15 09:29:37 +0700

    Time: 36 ms (76.81%), Space: 15.4 MB (90.79%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -4,6 +4,20 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    '''
+        Version: 1
+        Idea: Two pointer
+        1. One points to current node, initially head
+            -> when moved all, this becomes null
+        2. One points to previous, initially null
+            -> when moved all, this becomes the last node,
+            Meaning the new reversed head
+        3. Repeat:
+            - save current node.next (since link will be lost)
+            - curr node.next = prev (reverse)
+            - move previous: previous = curr
+            - move curr: curr = saved next
+    '''
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         revHead = None
         while head:
