c69f54ab9ba0ebee04ebe15672f615582cc413dc
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-02-17 08:37:32 +0700

    Time: 86 ms (87.30%), Space: 24.2 MB (26.49%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -4,6 +4,21 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    '''
+        Version: 1
+        Idea:
+            1. Use a double-headed queue
+                And keep popping from left, right
+                Constructing our result
+            2. Start with a dummy node to remove
+                edge cases
+        Complexity:
+        - Time: O(2n)
+            -> Create queue n
+            -> Pop n
+        - Space: O(n)
+            -> Queue contains ref to our list
+    '''
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
