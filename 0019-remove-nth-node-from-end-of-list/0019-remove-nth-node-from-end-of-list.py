efef340530fb444e4f6d1b34ae6f2ec0f32fa50f
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-02-18 10:36:31 +0700

    Time: 38 ms (48.64%), Space: 13.8 MB (97.59%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -4,6 +4,15 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    '''
+        Version: 1
+        Idea:
+        1. Use two pointer n-nodes appart to find target node
+        2. Use prev pointer to bridge the gap
+        Complexity:
+        - Time: O(n)
+        - Space: O(1)
+    '''
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         end = head
         for _ in range(n):
