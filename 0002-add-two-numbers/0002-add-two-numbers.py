5300a021ce722baf602829b7dd79db0295759f81
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-02-19 12:04:38 +0700

    Time: 75 ms (42.93%), Space: 13.9 MB (35.40%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,9 +4,22 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    '''
+        Version: 1
+        Idea:
+        1. Merge using digit sum + remainder carry through
+        2. Merge until one list runs out and continue with leftover
+        3. Use dummy to remove complication
+        4. Make sure to add one more carry through if remainder is
+            not 0 at the end
+        Complexity:
+        - Time: O(max(m,n))
+        - Space: O(1)
+    '''
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         cur = dummy = ListNode()
         remain = 0
+
         while l1 and l2:
             val1 = l1.val
             val2 = l2.val
@@ -19,13 +32,14 @@ class Solution:
             l1 = l1.next
             l2 = l2.next
         
-        remaining_list = l1 if l1 else l2
-        while remaining_list:
-            val = remaining_list.val + remain
+        leftover = l1 if l1 else l2
+        while leftover:
+            val = leftover.val + remain
             cur.next = ListNode(val % 10)
             remain = val // 10
-            remaining_list = remaining_list.next
-            cur = cur.next
-        if remain: cur.next = ListNode(remain)
 
+            leftover = leftover.next
+            cur = cur.next
+        
+        if remain > 0: cur.next = ListNode(remain)
         return dummy.next
