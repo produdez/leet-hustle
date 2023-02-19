77f081cac3799928288d13ed6ab31b7fcb507193
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-02-19 13:15:01 +0700

    Time: 57 ms (67.71%), Space: 17.5 MB (94.19%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -5,6 +5,15 @@
 #         self.next = None
 
 class Solution:
+    '''
+        Version: 1
+        Idea:
+            Use slow/fast pointer (turtle and hare)
+                for cycle detection
+        Complexity:
+        - Time: ??
+        - Space: O(1)
+    '''
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head: return False
         slow, fast = head, head.next
