48da724e2c262ccbcca30dc5ce205c6b8d4b638d
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-03-12 12:12:10 +0700

    Time: 40 ms (14.09%), Space: 13.8 MB (94.47%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -5,71 +5,33 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    '''
-        Version 3.5:
-            Refac code
-            Morris traversal
-            -> No stack / No Recursion
-        Idea:
-            Add additional pointer to guide our traversal
-            - DFS -> We're traveling all left then right 
-            - And swapping (inversing) on every node we visit
-            - * Main idea: Every time we attempt to traverse to next node (left)
-                We need to make sure the last node visited on the sub-tree that we travel
-                have a pointer back to the current node
-                -> Meaning we set a path from last visited to current
-            - When hit deadend -> We go back to parent and traverse the right branch (cause dfs)
-            - If there's no right branch on any parent -> End cause the treee is left lopsided
-            - If there's no parent -> We have ended on the last node on the right of the tree
-                -> END
-                
-            * NOTE *
-            - Since we're swapping at every node 
-            -> The last node we visit turns out to be the left-most child
-            -> Why?
-                1. Since we inverse the tree and do dfs, last node should be the 
-                    inverse of the predecesor node
-                    * pred node is right most on left branch
-                    -> inverse -> left most on right branch
-                2. BUT * big but since we only inverse one level at a time
-                    -> the node we're looking for at the time that level is inversed
-                    -> will be left most on left branch -> left most node!
-        Complexity:
-        - Time: O(2n) (Extra time to find decendant) -> O(n)
-        - Space: Extra space on the tree itself
-            * If parent path is deleted after use -> O(logN) or O(d)
-            * else it's just O(n)
-            
-    '''
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def descendant(root): 
-            # the code make sure root here always have left
-            while root.left: root = root.left
+        def left_most(root):
+            temp = root.left
+            if not temp: return None
+            while temp.left and temp.left != root: temp = temp.left
+            return temp
+        
+        def descendant(root):
+            root = root.left
+            while root.right: root = root.right
             return root
 
         curr = root
         while curr:
-            # swap
-            curr.left, curr.right = curr.right, curr.left
-
-            if curr.left:
-                # set link from last decendant to parent
-                descendant(curr).parent = curr
+            # print(f'at: {curr.val}')
+            cycle = left_most(curr)
+            # print(f'left_most of {curr.val} is {cycle.val if cycle else None}')
+            if not cycle or cycle.left == curr: # means have cycle
+                # swap
+                if cycle: cycle.left = None
+                # print(f'swapping at: {curr.val}')
+                curr.left, curr.right = curr.right, curr.left
                 curr = curr.left
-            elif curr.right:
-                curr = curr.right
             else:
-                right_branch = None
-                while hasattr(curr, 'parent'):
-                    parent = curr.parent
-                    delattr(curr, 'parent') # delete link
-                    curr = parent
-                    if curr.right: 
-                        right_branch = curr.right
-                        break
-
-                curr = right_branch
-        
+                # link
+                des = descendant(curr)
+                des.right = curr
+                # print(f'descendant of {curr.val} is {des.val}')
+                curr = curr.left
         return root
-
-        
