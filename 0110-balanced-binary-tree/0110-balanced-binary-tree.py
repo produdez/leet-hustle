2f61f59c71461862bc2ef1da0d2d3c23a49cad17
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-03-20 10:46:51 +0700

    Time: 47 ms (88.03%), Space: 14.7 MB (99.68%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,23 +6,28 @@
 #         self.right = right
 class Solution:
     '''
-        Version: 1
+        Version: 2
         Idea: 
-            Recursive post-order + global var to keep track of balance-ness of all nodes
+            Iterative post-order
         Complexity:
         - Time: O(n)
         - Space: O(d) - worse O(n)
     '''
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        balanced = [True]
-        def depth(root):
-            if not root: return -1
-            leftHeight = depth(root.left)
-            rightHeight = depth(root.right)
-            
-            if abs(leftHeight - rightHeight) > 1: 
-                balanced[0] = False
-            return 1 + max(leftHeight, rightHeight)
-        
-        depth(root)
-        return balanced[0]
+        if not root: return True
+        stack = [root]
+        depth = {None: -1}
+        while stack:
+            node = stack[-1]
+            if node.left in depth and node.right in depth:
+                stack.pop()
+                if abs(depth[node.left] - depth[node.right]) > 1:
+                    return False
+                
+                depth[node] = 1 + max(depth[node.left], depth[node.right])
+                if node.left: del depth[node.left]
+                if node.right: del depth[node.right]
+            else:
+                if node.left: stack.append(node.left)
+                if node.right: stack.append(node.right)
+        return True
