41606645bb7e7c6c8a328e737a62ba2fe759dba3
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-03-21 16:29:41 +0700

    Time: 81 ms (65.08%), Space: 18.7 MB (96.50%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,6 +6,18 @@
 #         self.right = None
 
 class Solution:
+    '''
+        Version: 1
+            Recursive
+        Idea:
+            Since BST, a node that's between p <= node <= q 
+            is the LCA
+            Cause if you move right, you loose the smaller node as a child
+            and if move left, loose the larger node as a child
+        Complexity:
+        - Time: O(d)
+        - Space: O(d) call stack
+    '''
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if root.val < p.val and root.val < q.val:
             return self.lowestCommonAncestor(root.right, p, q)
