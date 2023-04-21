54f09fb2577d72d49e5f99ed93b8b727e9727a70
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-21 16:44:35 +0700

    Time: 33 ms (75.80%), Space: 14.2 MB (25.10%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,8 +1,19 @@
 class Solution:
+    '''
+        Version 2:
+            DFS properly
+        
+    '''
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
-        def expand(nums, prev_sets=[[]]):
-            if not nums: return prev_sets
-            return expand(nums[1:], prev_sets + [s + [nums[0]] for s in prev_sets])
-        
-        return expand(nums)
+        result = []
+        def add(nums, start=[]):
+            if not nums: 
+                nonlocal result
+                result.append(start)
+                return
+            
+            add(nums[1:], start + [nums[0]])
+            add(nums[1:], start)
+            
+        add(nums)
+        return result
