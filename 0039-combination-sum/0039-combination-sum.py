a2bbfb35db4cbbdca9252594c018750a02bb2122
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-21 21:59:49 +0700

    Time: 52 ms (87.43%), Space: 14 MB (54.56%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        results = []
+        vals, csum = [], 0
+        
+        
+        def dfs(i):
+            nonlocal csum
+            if i >= len(candidates) or csum + candidates[i] > target:
+                return False
+            
+            csum += candidates[i]
+            
+            vals.append(candidates[i])
+            if csum == target: results.append(vals.copy())
+
+            while dfs(i): i += 1 #early stop
+
+            csum -= vals.pop()
+            return True
+        
+        if target == 0: results.append([])
+        for i in range(len(candidates)):
+            dfs(i)
+        return results 
