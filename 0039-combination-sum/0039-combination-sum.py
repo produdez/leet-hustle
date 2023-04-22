021a2de826c5d1d932003754debdd9112fc22c94
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-22 15:30:46 +0700

    Time: 50 ms (91.04%), Space: 14 MB (54.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,30 +1,30 @@
 class Solution:
     '''
-        Version: 1
-            My version of dfs, not very clean but efficient
+        Version: 2
+            Binary dfs
     '''
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         results = []
-        vals, csum = [], 0
         
-        
+        comb, csum = [], 0
         def dfs(i):
             nonlocal csum
-            if i >= len(candidates) or csum + candidates[i] > target:
+            if csum == target:
+                results.append(comb.copy())
+                return False
+            
+            if i >= len(candidates) or csum > target: 
                 return False
             
             csum += candidates[i]
-            
-            vals.append(candidates[i])
-            if csum == target: results.append(vals.copy())
-
-            while dfs(i): i += 1 #early stop
-
-            csum -= vals.pop()
-            return True
-        
-        if target == 0: results.append([])
-        for i in range(len(candidates)):
-            dfs(i)
+            if csum <= target:
+                comb.append(candidates[i])
+                dfs(i)
+                csum -= comb.pop()
+                dfs(i+1)
+            else:
+                csum -= candidates[i]
+    
+        dfs(0)
         return results 
