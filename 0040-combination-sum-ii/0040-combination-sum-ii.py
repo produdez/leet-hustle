673513b028485332c74179d3a785a5b086a653a1
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-23 15:40:42 +0700

    Time: 43 ms (93.47%), Space: 13.9 MB (48.89%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,40 +1,30 @@
 class Solution:
     '''
-        Version: 1
-            Binary dfs with early stop
+        Version: 2
+            N-ary dfs (simpler code)
     '''
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         res = []
-        nums = []
-        csum = 0
         
-        def dfs(i):
-            nonlocal csum
-            if csum == target: 
-                res.append(nums[::])
-                return False
+        def backtrack(pos, target, nums = []):
+            if target == 0: res.append(nums.copy())
+                
+            if target <= 0: return False # done
             
-            if i >= len(candidates) or csum > target:
-                return False
-            
-            if csum + candidates[i] > target: # saves abit of computation
-                return True
-            
-            nums.append(candidates[i])
-            csum += candidates[i]
-            valid = dfs(i+1)
-            csum -= nums.pop()
-            
-            # early stop
-            if not valid: return True 
-            
-            # avoid duplicates
-            while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
-                i += 1
-            dfs(i+1)
+            prev = -1 
+            for i in range(pos, len(candidates)):
+                if candidates[i] == prev: continue # exclude duplicates
+                
+                nums.append(candidates[i])
+                valid = backtrack(i + 1, target - candidates[i], nums)
+                nums.pop()
+                
+                if not valid: break
+                prev = candidates[i]
             
             return True
-
-        dfs(0)
+        
+        backtrack(0, target)
         return res
+        
