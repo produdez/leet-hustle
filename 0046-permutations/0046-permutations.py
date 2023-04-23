65e8296c44b716c53276033884e7b64aab3a67e1
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-23 09:42:03 +0700

    Time: 41 ms (64.75%), Space: 13.9 MB (95.16%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,19 @@
 class Solution:
     '''
-        Version: 1
-            DFS with smart but confusing structure
+        Version: 2
+            More intuitive
         Idea:
-            Basically, permu of list N (sized N)
-            = (permu of list N - an element) + append that element at the end
-            For every element in N
+            Perm(listN) = fixed_position_character + perm(listN - character)
+            for every char in listN
     '''
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 1: return [nums]
-            
-        result = []
-        for _ in range(len(nums)):
-            exclude = nums.pop(0)
-            
-            permute_excluded = self.permute(nums)
-            result.extend([perm + [exclude] for perm in permute_excluded])
-            
-            nums.append(exclude)
         
-        return result
+        def dfs(perm = [], nums= []):
+            if not nums: return [perm]
+            
+            res = []
+            for i in range(len(nums)):
+                res += dfs(perm + [nums[i]], nums[:i] + nums[i+1:])
+            return res
+        
+        return dfs([], nums)
