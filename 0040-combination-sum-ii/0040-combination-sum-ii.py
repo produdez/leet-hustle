b714678ee0b86ee2ff25cf6b6ed90368dd8e83a6
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-23 14:54:38 +0700

    Time: 50 ms (81.07%), Space: 14 MB (15.25%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,4 +1,8 @@
 class Solution:
+    '''
+        Version: 1
+        
+    '''
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         res = []
@@ -12,18 +16,25 @@ class Solution:
                 return False
             
             if i >= len(candidates) or csum > target:
-                # csum + candidates[i] > target
                 return False
+            
+            if csum + candidates[i] > target: # saves abit of computation
+                return True
             
             nums.append(candidates[i])
             csum += candidates[i]
             valid = dfs(i+1)
-            csum -= nums.pop() # pop before trying stuffs  wrong
-            if not valid: return True
+            csum -= nums.pop()
+            
+            # early stop
+            if not valid: return True 
+            
+            # avoid duplicates
             while i+1 < len(candidates) and candidates[i] == candidates[i+1]:
                 i += 1
             dfs(i+1)
             
             return True
+
         dfs(0)
         return res
