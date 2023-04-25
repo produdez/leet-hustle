96b40ace0fc5120a4a6075f5ec27368caf2cb615
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-25 17:19:00 +0700

    Time: 806 ms (5.01%), Space: 40.9 MB (5.01%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -4,31 +4,38 @@ class Solution:
             Backtrack split with index only not string slicing
     '''
     def partition(self, s: str) -> List[List[str]]:
+        res = []
+        splits = []
+
         def isPalin(start, end):
             for i in range(start, (start + end + 1) // 2):
                 if s[i] != s[start + end - i]: return False
             return True
-
-        def split(start=0):
-            res = []
-            for i in range(start, len(s)):
-                if isPalin(start, i): 
-                    remain = i+1
-                    if remain >= len(s): 
-                        res.append([i])
-                        break
-                    
-                    remain_splits = split(remain)
-                    for splits in remain_splits:
-                        res.append([i] + splits)
-                        
-            return res
         
-        res = []
-        for splits in split():
+        def gen_splitted(splits):
             lst = [s[:splits[0]+1]]
             for i in range(1, len(splits)):
                 lst.append(s[splits[i-1]+1 : splits[i] + 1])
             res.append(lst)
+            
+        def split(start=0):
+            if start >= len(s):
+                gen_splitted(splits)
+                return
+
+            for i in range(start, len(s)):
+                if isPalin(start, i): 
+                    splits.append(i)                    
+                    split(i+1)
+                    splits.pop()
+                                
+        # res = []
+        # split()
+        # for splits in split_cases:
+        #     lst = [s[:splits[0]+1]]
+        #     for i in range(1, len(splits)):
+        #         lst.append(s[splits[i-1]+1 : splits[i] + 1])
+        #     res.append(lst)
+        split()
         return res
                 
