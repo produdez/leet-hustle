64a415e1dc716cd82e6d53e3c0229dc892a0dbbe
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-25 16:41:10 +0700

    Time: 723 ms (13.18%), Space: 29.1 MB (77.67%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,14 +2,15 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         def isPalin(s):
             if len(s) == 1: return True
-            return s == s[::-1]
+            for i in range(len(s) // 2):
+                if s[i] != s[len(s) - (i+1)]: return False
+            return True
+
         def split(s, start=0):
             res = []
             for i in range(start, len(s)):
                 head = s[: i+1]
-                # print('head: ', head)
                 if isPalin(head): 
-                    # print('is palin')
                     remain = s[i+1:]
                     if not remain: 
                         res.append([head])
