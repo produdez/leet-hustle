9332d1af559fc0ad82aec63e637012755c214a8f
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-25 15:11:02 +0700

    Time: 1570 ms (87.27%), Space: 14 MB (43.21%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,10 +1,9 @@
 class Solution:
     '''
-        Version: 1.5
-            Same as version 1 
-            but slight cleaner backtrack code
-            and uses generator
-        Same efficient and space when summited
+        Version: 1.9
+            Now uses just an integer to represent index
+            y = idx // len(board[0])
+            x = idx % len(board[0])
     '''
     def exist(self, board: List[List[str]], word: str) -> bool:
         POS_MOVES = [
@@ -13,17 +12,28 @@ class Solution:
             (-1, 0), # left
             (0, 1), # up
         ]
-            
+
+        def print_board(board):
+            ender = '---------'
+            print(ender)
+            for line in board:
+                for char in line:
+                    print(char, end=', ')
+                print()
+            print(ender)
+
         def next_valid_positions(path):
             if len(path) < 1:
-                for i in range(len(board[0])):
-                    for j in range(len(board)):
-                        if board[j][i] == word[0]: yield (i,j)
+                total_board_size = len(board) * len(board[0])
+                i = 0
+                while i < total_board_size:
+                    if board[i // len(board[0])][i % len(board[0])] == word[0]: yield i
+                    i += 1
                 return
 
             if len(path) >= len(word): return
             
-            cur_x, cur_y = path[-1]
+            cur_x, cur_y = path[-1] % len(board[0]), path[-1] // len(board[0])
             for dx, dy in POS_MOVES:
                 new_x, new_y = cur_x + dx, cur_y + dy
                 
@@ -34,21 +44,32 @@ class Solution:
                 # check matching character
                 if board[new_y][new_x] != word[len(path)]: continue
                 
+                new_idx = new_y * len(board[0]) + new_x
                 # check repeated backtrack
-                if (new_x, new_y) in path: continue
+                if new_idx in path: continue
                 
-                yield (new_x, new_y)
+                yield new_idx
             return
 
         def backtrack(path = []):
+            # print('path: ', path)
             for next_pos in next_valid_positions(path):
                 path.append(next_pos)
                 if backtrack(path): return True
                 path.pop()
             
-            if len(path) == len(word): return True
+            if len(path) == len(word): 
+                # print('Final path:' , path)
+                res = ''
+                for pos in path:
+                    i, j = pos % len(board[0]), pos // len(board[0])
+                    res += board[j][i]
+                # print('word: ', res)
+                return True
             return False
 
         
         if set(word) - set([i for row in board for i in row]): return False
+        # print_board(board)
+        # print('word: ', word)
         return backtrack()
