8c3d347f3642b3f25a25c6fb231b7fa11cdbe7d8
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-27 11:21:28 +0700

    Time: 44 ms (7.54%), Space: 16.3 MB (5.90%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,26 @@
+class Solution:
+    '''
+        Version: 1
+            Memoized Recursion
+        Idea:
+            So u can climb 1 step or 2 steps
+            Meaning ways to climb n steps
+            = ways(n-1) # means try climb 1 step and climb the rest 
+            + ways(n-2) # means try climb 2 step and climb the rest
+            => This creates a tree with many repeated steps
+    '''
+    def climbStairs(self, n: int) -> int:
+        ways = [None] * n
+        
+        def recurClimb(n):
+            if n == 1: res = 1
+            elif n == 2: res = 2
+            else:
+                if ways[n-1] is not None:
+                    return ways[n-1]
+                res = recurClimb(n-1) + recurClimb(n-2)
+
+            ways[n-1] = res
+            return res
+        
+        return recurClimb(n)
