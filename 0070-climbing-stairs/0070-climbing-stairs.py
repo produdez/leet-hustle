b001182d512e56d49ba40d769bbbdd32cd8712a2
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-04-27 11:28:47 +0700

    Time: 57 ms (7.54%), Space: 16.2 MB (5.90%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -11,9 +11,7 @@ class Solution:
     '''
     def climbStairs(self, n: int) -> int: 
         if n == 1: return 1
-        ways = [None] * n
-        ways[0] = 1
-        ways[1] = 2
+        ways = [1,2]
         for i in range(2, n):
-            ways[i] = ways[i-1] + ways[i-2]
+            ways.append(ways[i-1] + ways[i-2])
         return ways[n-1]
