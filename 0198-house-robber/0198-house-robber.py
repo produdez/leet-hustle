6ed81cdf80a9735e83712a0da5c307456e35cfab
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-05-12 16:41:01 +0700

    Time: 39 ms (32.55%), Space: 16.3 MB (15.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,12 +3,10 @@ class Solution:
         if len(nums) == 1: return nums[0]
         prev, cur = nums[0], max(nums[0],nums[1])
         for i in range(2, len(nums)):
-            print(prev,cur)
             temp = cur
             cur = max(
                 cur,
                 prev + nums[i]
             )
             prev = temp
-        # print(prev, cur)
         return cur
