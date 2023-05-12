6db83481cd3b02c20d3d04c502f117492251ea2a
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-05-12 16:42:34 +0700

    Time: 52 ms (7.56%), Space: 16.2 MB (21.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
-        prev, cur = nums[0], max(nums[0],nums[1])
-        for i in range(2, len(nums)):
+        prev, cur = 0, 0
+        for val in nums:
             temp = cur
-            cur = max(
-                cur,
-                prev + nums[i]
-            )
+            cur = max(cur, prev + val)
             prev = temp
         return cur
