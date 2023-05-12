72c338f1933f321e290db1a1fd5ec90e15f0cd14
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-05-12 16:50:22 +0700

    Time: 47 ms (14.59%), Space: 16.3 MB (6.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,4 +1,14 @@
 class Solution:
+    '''
+    
+        Version: 3?
+            I did not note the first few versions
+            Dynamic with no edge cases
+        Idea:
+            - Cur_rob = Max(Prev_2_rob + curHOuse or Prev_1_rob)
+            - Init prev, cur as 0 to remove 2 edge cases
+            - use python built in swap for cleaner code
+    '''
     def rob(self, nums: List[int]) -> int:
         prev, cur = 0, 0
         for val in nums:
