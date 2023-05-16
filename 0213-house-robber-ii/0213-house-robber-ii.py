9390851c1d8e7f10ed649bd84b03d84a0fb25e72
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-05-16 14:52:05 +0700

    Time: 57 ms (5.03%), Space: 16.2 MB (28.20%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,10 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1: return nums[0]
+
         def rob_non_cyclic(start, end):
             before, prev = 0, 0
-            for val in nums[start: end]:
-                before, prev = prev, max(before + val, prev)
+            for i in range(start, end):
+                before, prev = prev, max(before + nums[i], prev)
             return prev
         
         return max(
