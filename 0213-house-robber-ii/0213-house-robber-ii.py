3e51db5597da765315014db6aee2a43df7ecf9ff
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-05-16 14:56:03 +0700

    Time: 53 ms (8.41%), Space: 16.4 MB (8.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,4 +1,19 @@
 class Solution:
+    '''
+        Version: 1
+            DP with trick
+        Idea:
+            Since we cannot rob the first and the last house at the same time
+            We just compare the two cases
+            - Rob all except first
+            - Rob all except last
+            And see which one is better
+            Cause it's very hard to keep track if we took the first house or not
+            so we just split it into two separated cases.
+        Complex: 
+        - Time: O(2n) = O(n)
+        - Space: O(1)
+    '''
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1: return nums[0]
 
