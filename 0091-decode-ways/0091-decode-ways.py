7387017d2a2aeab65a594691165e9e328c2cc5f6
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-05-29 10:35:33 +0700

    Time: 57 ms (8.14%), Space: 16.4 MB (25.67%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,34 +1,30 @@
 class Solution:
     '''
-        Version: 1.5
-            Update: add base case of countDec(len(s)) == 1
-            Backtracking with DP memoize
+        Version: 2
+            Bottom up backtracking with O(1) space
         Idea:
             Every case can be split 1 char or 2 char
             So check all cases (n^2)
             But since there's repeated work, we use memoize
         Complexity: 
         - Time: O(n) - reduced from O(n^2) of backtracking
-        - Space: O(n) - result of decoding at each index is saved
+        - Space: O(1) - result of decoding at each index is saved
     '''
     def numDecodings(self, s: str) -> int:
-        memoize = {len(s): 1}
-        
-        def countDec(start):
-            if start in memoize: return memoize[start]
-            if start >= len(s) or s[start] == '0': total = 0
-            else:
-                total = countDec(start+1)
-                if start + 1 < len(s) and \
-                    (
-                        s[start] == '1' or 
-                        (s[start] == '2' and s[start+1] in '0123456')
-                    ):
-                    total += countDec(start+2)
+        # this is for idx len(s - 1) and len(s)
+        nxt, nxt2 = (0, 1) if s[-1] == '0' else (1, 1)
+        def countDecRev(start):
+            nonlocal nxt, nxt2
+            if start < 0: return nxt
             
-            memoize[start] = total
-            return total
-        
-        
-        return countDec(0)
+            total = 0
+            if s[start] != '0': total += nxt
+            if s[start] == '1' or \
+                (s[start] == '2' and s[start+1] in '0123456'):
+                total += nxt2
+            
+            nxt, nxt2 = total, nxt
+            return countDecRev(start - 1)
+            
+        return countDecRev(len(s) - 2)
         
