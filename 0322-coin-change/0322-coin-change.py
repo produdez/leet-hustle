da5d3e758e095b0eb07869d21f5500f67ddaf6a8
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-06-05 11:28:44 +0700

    Time: 1513 ms (36.99%), Space: 16.7 MB (58.80%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,7 +1,11 @@
 class Solution:
     '''
-        Version: 2
+        Version: 2.5
             Bottom up DP
+            Update:
+            - Use array with amount + 1 as MAX
+            - No need to pre-initialize since that will happen
+                as we bottom up and amount = 0
         Idea:
             Work up from amount = 0 to amount = n
         Complex:
@@ -9,24 +13,20 @@ class Solution:
             Space: O(n)
     '''
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0: return 0
-        coins.sort()
-        memoize = {i: 1 for i in coins}
+        memoize = [amount + 1] * (amount + 1)
         memoize[0] = 0
-        
-        def bottomUp(amount):
-            if amount in memoize: return amount
-            
-            res = math.inf
+        coins.sort()
+        for amt in range(1, amount + 1):
             for coin in coins:
-                if amount < coin: break
-                if amount - coin in memoize:
-                    res = min(memoize[amount - coin] + 1, res)
+                if amt < coin: break
+                # dont even need to check if it's in memoize
+                # cause amt - coin always in memoize is amt >= coin
+                    
+                # this is possible since we go bottom up and if 
+                # mem[amt-coin] is not found means it's MAX and max + 1 
+                # is always larger than our current memoize[amt]
+                memoize[amt] = min(memoize[amt-coin] + 1, memoize[amt])
+                if memoize[amt-coin] == 0: break # best case
             
-            if res != math.inf: memoize[amount] = res
         
-        for i in range(1, amount + 1):
-            bottomUp(i)
-        
-        if amount in memoize: return memoize[amount]
-        return -1
+        return memoize[amount] if memoize[amount] != amount + 1 else -1
