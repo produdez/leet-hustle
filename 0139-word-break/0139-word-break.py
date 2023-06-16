8de273bde479d90940fb2a14cbc7688c00fe858c
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-06-16 10:30:45 +0700

    Time: 55 ms (50.93%), Space: 16.6 MB (26.21%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,4 +1,18 @@
 class Solution:
+    '''
+        Version: 2
+            Bottom Up DP
+        Idea:
+            Build up our wordBroken str from it's wordBroken substr
+            from index n to index 0
+            
+            From right to left, check all words in dictionary
+            memoize[i] = match and memoize[i: i + len(matching_word)] (at least 1)
+        Complexity:
+        - Time: O(N * NW * W): check all pos (n) for all words (WD) and each word
+            comparison takes avg word size (W)
+        - Space: O(n) memoize
+    '''
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         memoize = [None] * (len(s) + 1)
         memoize[-1] = True
@@ -8,7 +22,7 @@ class Solution:
             for word in wordDict:
                 if s[i] == word[0] and s[i: i+len(word)] == word: 
                     matched = memoize[i + len(word)]
-                    if matched is True: break
+                    if matched: break
             memoize[i] = matched
         
         for i in reversed(range(0, len(s))):
