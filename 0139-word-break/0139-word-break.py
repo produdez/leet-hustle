5452ae4349f3b4ee77f6f1f36d43c9f359292713
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-06-16 10:26:11 +0700

    Time: 43 ms (88.82%), Space: 16.4 MB (47.51%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,34 +1,16 @@
 class Solution:
-    '''
-        Version: 1
-            Cached (Top down DP)
-        Idea:
-            check each word with the string and save result
-            Meaning if s[start : len(s)] is valid/or invalid
-            we'll save it and save the computation
-        Complextiy:
-        - Time: O(NW * W * N) 
-            with NW: num_word, W: word_count, N: str_len
-            If no caching then time is
-            O(NW^n * M * N) with n aprox N and is depth of deci tree with
-            branching factor NW
-        - Space: O(n) memoize
-    '''
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:          
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         memoize = [None] * (len(s) + 1)
         memoize[-1] = True
         
-        def check(start):
-            if memoize[start] is not None: return memoize[start]
-            
-            res = False
+        def check(i):
+            matched = False
             for word in wordDict:
-                if word[0] == s[start] and word == s[start: start + len(word)]:
-                    if check(start + len(word)): 
-                        res = True
-                        break
-            memoize[start] = res
-            return res
+                if s[i] == word[0] and s[i: i+len(word)] == word: 
+                    matched = memoize[i + len(word)]
+                    if matched is True: break
+            memoize[i] = matched
         
-        return check(0)
-                
+        for i in reversed(range(0, len(s))):
+            check(i)
+        return memoize[0]
