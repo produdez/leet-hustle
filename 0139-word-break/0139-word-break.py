d1a78d99d983070c72c5e0b559ba38b34e138a98
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-06-16 09:56:49 +0700

    Time: 50 ms (71.04%), Space: 16.7 MB (8.77%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,4 +1,19 @@
 class Solution:
+    '''
+        Version: 1
+            Cached (Top down DP)
+        Idea:
+            check each word with the string and save result
+            Meaning if s[start : len(s)] is valid/or invalid
+            we'll save it and save the computation
+        Complextiy:
+        - Time: O(NW * W * N) 
+            with NW: num_word, W: word_count, N: str_len
+            If no caching then time is
+            O(NW^n * M * N) with n aprox N and is depth of deci tree with
+            branching factor NW
+        - Space: O(n) memoize
+    '''
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:          
         memoize = [None] * (len(s) + 1)
         memoize[-1] = True
