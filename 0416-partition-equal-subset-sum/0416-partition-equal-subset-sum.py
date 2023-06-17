b3ac109d4c5eea47c9cf3069c11aecd4a800dde4
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-06-17 11:19:11 +0700

    Time: 3028 ms (24.21%), Space: 492.8 MB (5.00%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,4 +1,30 @@
 class Solution:
+    '''
+        Version: 1
+            Recursive DFS with caching for armotized complexity
+            Basically topdown dynamic cheat
+        Idea:
+            1. Base idea is you find subset that sums to half of the original
+                subset
+            2. And you keep track by the starting index of subset and target 
+                that you're summing to
+            
+            CHEAT: use caching
+            1. Basically, dfs will check all combination, meaning for each element
+                we have 2 choice: include/disclude -> O(2^n) time complex
+            2. But, when using cache, the complexity reduces to the complexity of
+                the cache. 
+            3. And since the cache made up from (index, target) which has complex
+                O(n * sum(nums))
+                This caps the complextity down to O(n * sum(nums))
+            4. And given the constrait that nums.length >> nums[i]
+                Meaning 2^n >>>>>> O(n*sum(nums))
+            We get a comparable result @@
+            
+            Complexity:
+            - Time: O(n * sum(nums))
+            - Space: Same
+    '''
     def canPartition(self, nums: List[int]) -> bool:
         memoize = {}
         
