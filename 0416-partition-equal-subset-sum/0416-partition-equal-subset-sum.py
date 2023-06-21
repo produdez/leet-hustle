c6557afb056d1258adc059e10ea721089c7ef70a
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-06-21 10:18:48 +0700

    Time: 1719 ms (40.63%), Space: 16.5 MB (83.10%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -2,6 +2,14 @@ class Solution:
     '''
         Version 2: 
             Bottom up 1d optimized 1/0 knapsack
+        Idea:
+            Build a 1d array representation of a 2d array for 1/0 knapsack
+            array[i][j] means can i sum to target j with first i elements from nums
+            array[i][j] = array[i-1][j] (sum to same target with less elem) 
+                or array[i-1][j-nums[i]] (sum to target reduced by current element)
+        Complexity:
+        - Time: O(n * sum(nums))
+        - Space: O(sum(nums)) or O(n * sum(nums)) if not optimized
     '''
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
