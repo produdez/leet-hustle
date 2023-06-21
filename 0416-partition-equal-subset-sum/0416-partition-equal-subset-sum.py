c69f1bf209e0589f374f1ff6143e27efd5839897
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-06-21 08:37:07 +0700

    Time: 3054 ms (23.45%), Space: 575.9 MB (5.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,44 +1,25 @@
 class Solution:
     '''
-        Version: 1
-            Recursive DFS with caching for armotized complexity
-            Basically topdown dynamic cheat
-        Idea:
-            1. Base idea is you find subset that sums to half of the original
-                subset
-            2. And you keep track by the starting index of subset and target 
-                that you're summing to
-            
-            CHEAT: use caching
-            1. Basically, dfs will check all combination, meaning for each element
-                we have 2 choice: include/disclude -> O(2^n) time complex
-            2. But, when using cache, the complexity reduces to the complexity of
-                the cache. 
-            3. And since the cache made up from (index, target) which has complex
-                O(n * sum(nums))
-                This caps the complextity down to O(n * sum(nums))
-            4. And given the constrait that nums.length >> nums[i]
-                Meaning 2^n >>>>>> O(n*sum(nums))
-            We get a comparable result @@
-            
-            Complexity:
-            - Time: O(n * sum(nums))
-            - Space: Same
+        Continue: bottom up approach and complexity
     '''
     def canPartition(self, nums: List[int]) -> bool:
+        
         memoize = {}
         
-        def dfs(i, target):
+        def validPartition(i, target):
             if i >= len(nums): return False
-            if (i, target) in memoize: return memoize[(i, target)]
+            if (i, target) in memoize: return memoize[i,target]
+            if nums[i] == target: result = True
+            else:
+                result = (
+                    validPartition(i + 1, target) or
+                    validPartition(i + 1, target - nums[i])
+                )
             
-            res = nums[i] == target \
-                or dfs(i + 1, target - nums[i]) \
-                or dfs(i + 1, target)
-            
-            memoize[(i, target)] = res
-            return res
+            memoize[i, target] = result
+            return result
+        
         
         total = sum(nums)
-        if total % 2 != 0: return False
-        return dfs(0, total // 2)
+        if total % 2 != 0: return False 
+        return validPartition(0, total // 2)
