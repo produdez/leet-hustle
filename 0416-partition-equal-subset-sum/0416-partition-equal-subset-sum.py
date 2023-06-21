68d40d9643a88ab61f0d4ac92a9ebf8d3e5c1b64
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-06-21 09:29:48 +0700

    Time: 384 ms (94.18%), Space: 17.7 MB (69.02%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,24 +1,15 @@
 class Solution:
-    '''
-        Continue: bottom up approach and complexity
-    '''
     def canPartition(self, nums: List[int]) -> bool:
-        
         total = sum(nums)
         if total % 2 == 1: return False
-        queue = [(len(nums) - 1, total // 2)]
-        memoize = set()
         
-        while queue:
-            i, target = queue.pop(0)
-            if i < 0: continue
-            if (i,target) in memoize: continue
-            if target == nums[i]: return True
-            
-            memoize.add((i, target))
-            queue.append((i-1, target))
-            queue.append((i-1, target - nums[i]))
+        halfSum = total // 2
+        targets = {0}
         
+        for num in nums:
+            newTargets = []
+            for target in targets:
+                if target + num == halfSum: return True
+                newTargets.append(target + num)
+            targets.update(newTargets)
         return False
-            
-            
