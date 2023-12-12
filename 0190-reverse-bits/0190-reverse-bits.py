705127c1c0ff151f281574c81ee957bd1c1c5080
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-12-12 15:42:10 +0700

    Time: 45 ms (23.93%), Space: 16.3 MB (9.51%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,6 +1,12 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        # naive string approach
-        binary = str(bin(n))[2:]
-        rev = binary[::-1]
-        return int(rev, base=2)  << (32 - len(binary))
+        # math solution (iterate + add padding)
+        
+        if n == 0: return 0
+        bit_count = int(math.log2(n)) + 1
+        res = 0
+        while n > 0:
+            res = res << 1
+            res += n % 2
+            n = n >> 1
+        return res << (32 - bit_count)
