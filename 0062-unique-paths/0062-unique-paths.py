24edf6b6eb9250624c1189924fd9fd434ab48e7e
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-12-12 15:48:31 +0700

    Time: 34 ms (85.12%), Space: 16.4 MB (33.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,12 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # optimal?
+        # math solution
+        # idea: (m-1 + n-1) boxes to put (m-1) of the first element and (n-1) of the second element
+        # result? total way to arrange devide by duplicate
+        # duplicate? well (m-1) of first and switch with each other ans same for second element
+        # -> (m+n-2)! / (m-1)!(n-1)!
+        # simplification -> (m+n-2)C(m-1) - (or Cn-1)
         
-        m, n = (m,n) if m<n else (n,m)
-        memoize = [1] * n
-        diff = n-m
-        for i in range(m-2, -1, -1): 
-            memoize[i+diff] =  memoize[i+diff] * 2
-            for j in range(i+diff-1, -1, -1):
-                memoize[j] = memoize[j] + memoize[j+1]
-        return memoize[0]
+        return math.comb(m+n-2, m-1)
