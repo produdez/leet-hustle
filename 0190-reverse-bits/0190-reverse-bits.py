6c9a3febef36f7c899ec81d7e7392cff3d8e2524
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-12-12 15:41:45 +0700

    Time: 34 ms (89.04%), Space: 16.2 MB (40.42%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,8 +1,6 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        # naive string manip
-        
+        # naive string approach
         binary = str(bin(n))[2:]
-        padded = ('0' * (32 - len(binary))) + binary
-        rev = padded[::-1]
-        return int(rev, base=2)
+        rev = binary[::-1]
+        return int(rev, base=2)  << (32 - len(binary))
