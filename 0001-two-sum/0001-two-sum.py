63b476ec761322b951d61a7445238dfa1347e47f
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-12-12 15:30:13 +0700

    Time: 62 ms (76.35%), Space: 17.6 MB (43.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        key_bag = dict()
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in key_bag: return [i, key_bag.get(complement)]
-            key_bag[num] = i
+        visited = {}
+        for i, val in enumerate(nums):
+            complement = target - val
+            if complement in visited: return [i, visited[complement]]
+            visited[val] = i
         
-        raise Exception('No pair ever add up to the target')
+        return False
