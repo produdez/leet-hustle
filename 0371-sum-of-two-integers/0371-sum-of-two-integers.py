e0c0ffdba32603a0e722f45bb2b051275fd76374
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-12-13 14:05:28 +0700

    Time: 41 ms (31.23%), Space: 16.4 MB (14.32%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,18 +1,10 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
         '''
-            Translate from int (2's compliment) to one's complement
-            
-            Do calculation based on one's complement
-            
-            Translate back to two's complement
-            
-            Better approach is to learn how to do 2's complement (todo later)
-            toOne and toTwo should (could?) be one function
+            Implement 2's complement sum
         '''
-        
-        
-        MAX_INT = 0b11111111111 # one extra for sign and 10 for value since max is 1000
+
+
         lookupTable = {
             (1,1,1): (1,1),
             (1,1,0): (0,1),
@@ -26,39 +18,41 @@ class Solution:
             (0,0,0): (0,0),
             (0,0,1): (1,0),
         }
-        
-        
-        def toOne(i):
-            if i > 0: return i
-            return MAX_INT & (i - 1)
 
-        def toTwo(c1):
-            if c1.bit_length() < MAX_INT.bit_length(): return c1
-            return int('-'+bin(MAX_INT & ~c1),2) # Un-optimized
+        BIT_COUNT = 10
+
+        i,j,c = 0, 0,0
+        res = 0 # extra place hold bit
+        # print('---------------')
+        # print('starting: ', a,b)
+        # print('init res: ', bin(res))
+        count = 0
+        while (a or b) and count < BIT_COUNT + 1:
+            i = a & 1
+            j = b & 1
+
+            # print('i,j,c: ', i,j,c)
+            v, c = lookupTable[(i,j,c)]
+
+            # print('v,c: ', v,c)
+            res |= v << count
+
+            # print('res: ', res, bin(res))
+
+            a >>= 1
+            b >>= 1
+            count += 1
+
+        # print('final count: ', count)
+        if count < BIT_COUNT + 1:
+            # print('final adjust')
+            v, _ = lookupTable[(0,0,c)] # final carry
+            res |= v << count
 
 
-
-        def sum1c(a,b):
-            c = 0
-            # one extra redundant bit
-            res = 1 << MAX_INT.bit_length()
-            
-            for count in range(MAX_INT.bit_length()):
-                i = a%2
-                j = b%2
-
-                v, c = lookupTable[(i,j,c)]
-                res |= v << count
-                a >>= 1
-                b >>= 1
-
-            # remove bit at head
-            res = res ^ (1 << (res.bit_length() - 1)) 
-            
-            # must be a recursive call since we must run though all bits to know if carry stays
-            # might try an initialized while loop but fuck you bitHC as bit manupluatioavhasgfbalweuvvr,zdvasefez
-            if c: return sum1c(res,c) 
-            return res
-        
-        oneComplimentResult = sum1c(toOne(a), toOne(b))
-        return toTwo(oneComplimentResult)
+        # print('final result: ', res, bin(res))
+        if res >> BIT_COUNT:
+            res = ~((0b11111111111 & ~res) + 1) + 1
+        return res
+    
+    
