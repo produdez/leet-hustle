7158de45bd6ee360337a59a8052aaa6b41b2f7eb
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-12-16 17:07:44 +0700

    Time: 140 ms (42.33%), Space: 21.2 MB (48.91%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,24 @@
+from itertools import takewhile
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda i: i[0])
+        
+        res = []
+        cur = intervals[0]
+        
+        for nxt in intervals[1:]:
+            if cur[1] < nxt[0]:
+                res.append(cur)
+                cur = nxt
+            elif cur[0] <= nxt[1]:
+                cur = [
+                    min(cur[0], nxt[0]),
+                    max(cur[1], nxt[1])
+                ]
+            else:
+                res.append(nxt)
+                cur = None
+                
+        if cur: res.append(cur)
+        return res
