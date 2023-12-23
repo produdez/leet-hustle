34ad671a2609c07304425089c7021b7535ea5e1b
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-12-23 15:53:55 +0700

    Time: 29 ms (99.37%), Space: 17.5 MB (11.52%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,50 +1,18 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        """
-            Simple approach with updated rotate mechanism
-            1. Traverse a quadrant of the matrix
-            2. Repeat 4 times the rotation (cause there's 4 quadrants)
-            3. Rotation include:
-                a. translate the index to relative to the center
-                b. rotate using math formula
-                c. translate back to original index
-        """
-        
+        # different approach where we rotate each "layer" of our matrix
         n = len(matrix)
+        left, right = 0, len(matrix) - 1
+        top, bottom = 0, len(matrix) - 1
         
-        def translate(pos, origin, new_origin):
-            transpose_vector = (origin[0] - new_origin[0], origin[1] - new_origin[1])
-            new_pos = (pos[0] + transpose_vector[0], pos[1] + transpose_vector[1])
-            return new_pos    
-
-        def rotate(pos):
-            t_pos = translate(pos, origin, center)
-            # y, -x is rot 90 clock wise with x points down and y points right
-            t_rotated = (t_pos[1], -t_pos[0]) 
-            rotated = translate(t_rotated, center, origin)
-            return int(rotated[0]), int(rotated[1])
-        
-        
-        def get(grid, pos):
-            return grid[pos[0]][pos[1]]
-        
-        def first_quadrant():
-            offset = n%2 # add one more row in case of odd n
-            for i in range(n//2 + offset):
-                for j in range(n//2):
-                    yield (i,j)
-        
-        origin = (0,0)
-        center = ((n-1)/2, (n-1)/2)
-        for cur in first_quadrant():
-            cur_val = get(matrix, cur)
-            if cur == center: continue
-
-            for _ in range(4):
-                nxt = rotate(cur)
-                temp = get(matrix, nxt)
-                matrix[nxt[0]][nxt[1]] = cur_val
-                
-                cur = nxt
-                cur_val = temp
-        
+        while left < right:
+            for i in range(right - left):
+                temp = matrix[top][left+i]
+                matrix[top][left+i] = matrix[bottom-i][left]
+                matrix[bottom-i][left] = matrix[bottom][right-i]
+                matrix[bottom][right-i] = matrix[top+i][right]
+                matrix[top+i][right] = temp
+            left += 1
+            right -= 1
+            top += 1
+            bottom -= 1
