e281cb14a9fbd5c12834982bfa8cb1fb092d4037
Produde (Long) <72970246+produdez@users.noreply.github.com>
2023-12-29 20:42:12 +0700

    Time: 2071 ms (5.02%), Space: 56.2 MB (6.81%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,9 +3,9 @@ class Solution:
         removed = 0
         intervals.sort(key=lambda i: i[0])
         
-        prev = None
-        for interval in intervals:
-            if prev is None or prev[1] <= interval[0] or interval[1] <= interval[0]: 
+        prev = intervals[0]
+        for interval in intervals[1:]:
+            if prev[1] <= interval[0] or interval[1] <= interval[0]: 
                 prev = interval
             else:
                 prev = prev if prev[1] < interval[1] else interval
