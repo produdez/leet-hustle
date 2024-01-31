ae39ddf600d500cd2238045087688a90898f439b
Produde (Long) <72970246+produdez@users.noreply.github.com>
2024-01-31 17:37:59 +0700

    Time: 31 ms (89.71%), Space: 16.4 MB (69.81%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,7 +1,21 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
+        """
+            Trick explain:
+            1. n-1 will subtract one bit from n
+            2. n & (n-1) will unset the next valid bit
+            Example:
+            n   : 101000
+            n-1 : 100111
+            n & (n-1)
+            =   : 100000 (valid bit was remove)
+            
+            Complexity: O(32) aka O(1) since max size of num is 32 bit
+            But we actually iterate only the valid bit so there's best case and avg case
+        """
         count = 0
         while n:
-            count += n % 2
-            n >>= 1
+            count += 1
+            n = n & (n-1) # TRICK
+            
         return count
