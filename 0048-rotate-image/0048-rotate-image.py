ca5c23c6221f64a9f9027f32a72f9d43b938af5f
Produde (Long) <72970246+produdez@users.noreply.github.com>
2024-02-01 16:51:25 +0700

    Time: 42 ms (48.09%), Space: 16.7 MB (58.51%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,18 +1,16 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        # different approach where we rotate each "layer" of our matrix
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
         n = len(matrix)
-        left, right = 0, len(matrix) - 1
-        top, bottom = 0, len(matrix) - 1
+        for layer in range(n // 2): # there are n//2 layers size > 1
+            for i in range(n - layer*2 -1): # each slice of a square is layer length - 1
+                start,end = layer, n-layer-1
+                temp = matrix[start][start+i]
+                matrix[start][start+i] = matrix[end-i][start]
+                matrix[end-i][start] = matrix[end][end-i]
+                matrix[end][end-i] = matrix[start+i][end]
+                matrix[start+i][end] = temp
         
-        while left < right:
-            for i in range(right - left):
-                temp = matrix[top][left+i]
-                matrix[top][left+i] = matrix[bottom-i][left]
-                matrix[bottom-i][left] = matrix[bottom][right-i]
-                matrix[bottom][right-i] = matrix[top+i][right]
-                matrix[top+i][right] = temp
-            left += 1
-            right -= 1
-            top += 1
-            bottom -= 1
+        # inplace replace so no return
