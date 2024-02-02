7184616745d1b0a899660445f29084df997483d8
Produde (Long) <72970246+produdez@users.noreply.github.com>
2024-02-02 15:26:11 +0700

    Time: 347 ms (91.12%), Space: 18 MB (60.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,15 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        canEnd = [False] * n
-        canEnd[-1] = True
-        
-        for i in reversed(range(n-1)):
-            if nums[i] > n - i - 1:
-                canEnd[i] = True
-                continue
-            for jump in range(1, nums[i] + 1):
-                if canEnd[i + jump]:
-                    canEnd[i] = True
-                    break
-        return canEnd[0]
+        lastValid = n - 1
+        for i in reversed(range(lastValid)):
+            if nums[i] >= lastValid - i:
+                lastValid = i
+        return lastValid == 0
