98ce01c6fd32749b8b91682a40d78f7aa4c89571
Produde (Long) <72970246+produdez@users.noreply.github.com>
2024-09-08 17:25:00 +0700

    Time: 1210 ms (12.93%), Space: 46.5 MB (5.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,32 +1,22 @@
 class Solution:
-    '''
-        Version: 2.5
-            Bottom up DP
-            Update:
-            - Use array with amount + 1 as MAX
-            - No need to pre-initialize since that will happen
-                as we bottom up and amount = 0
-        Idea:
-            Work up from amount = 0 to amount = n
-        Complex:
-            Time: O(n * m) n~amount and m is avg#coin taken aka depth
-            Space: O(n)
-    '''
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memoize = [amount + 1] * (amount + 1)
-        memoize[0] = 0
-        coins.sort()
-        for amt in range(1, amount + 1):
-            for coin in coins:
-                if amt < coin: break
-                # dont even need to check if it's in memoize
-                # cause amt - coin always in memoize is amt >= coin
-                    
-                # this is possible since we go bottom up and if 
-                # mem[amt-coin] is not found means it's MAX and max + 1 
-                # is always larger than our current memoize[amt]
-                memoize[amt] = min(memoize[amt-coin] + 1, memoize[amt])
-                if memoize[amt-coin] == 0: break # best case
+        coins.sort() # small to large
+        mem = {0: 0}
+        def dp(target):
+            if target < 0: return -1
+            if target in mem: return mem[target]
             
+            res = float('inf')
+            for c in coins[::-1]:
+                r = dp(target - c)
+                if r < 0: continue
+                res = min(1 + r, res)
+            
+            res = -1 if res == float('inf') else res
+            mem[target] = res
+            return res 
+        dp(amount)
         
-        return memoize[amount] if memoize[amount] != amount + 1 else -1
+
+        # print(mem)
+        return mem[amount]
