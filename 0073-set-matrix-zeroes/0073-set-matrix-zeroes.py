5f4bb5884e8833489349f22d1994317db63e413c
Produde (Long) <72970246+produdez@users.noreply.github.com>
2024-09-08 14:50:42 +0700

    Time: 97 ms (85.11%), Space: 17.5 MB (57.05%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,25 +1,25 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        def setNegative(i,j, value=None):
-            # set to -1
-            matrix[i][j] = value   
-            for k in range(len(matrix)):
-                if matrix[k][j] == 0:
-                    setNegative(k,j)
-                matrix[k][j] = value
-            for k in range(len(matrix[0])):
-                if matrix[i][k] == 0:
-                    setNegative(i,k)
-                matrix[i][k] = value
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                val = matrix[i][j]
-                if val != 0: continue
-                setNegative(i,j)
-
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == None:
-                    matrix[i][j] = 0
         
-        return
+        first_row = 1
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    if i == 0:
+                        first_row = 0
+                    else:
+                        matrix[i][0] = 0
+
+        # Iterate the inner of the matrix as not to override the first row
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+        # remaining first row, col
+        if matrix[0][0] == 0:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+        if first_row == 0:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
