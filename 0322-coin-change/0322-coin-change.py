2dce55cd78027039f3d9c5f1a587033e8b9a1c97
Produde (Long) <72970246+produdez@users.noreply.github.com>
2024-09-08 17:38:50 +0700

    Time: 739 ms (76.68%), Space: 16.8 MB (94.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,22 +1,9 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort() # small to large
-        mem = {0: 0}
-        def dp(target):
-            if target < 0: return -1
-            if target in mem: return mem[target]
-            
-            res = float('inf')
-            for c in coins[::-1]:
-                r = dp(target - c)
-                if r < 0: continue
-                res = min(1 + r, res)
-            
-            res = -1 if res == float('inf') else res
-            mem[target] = res
-            return res 
-        dp(amount)
-        
-
-        # print(mem)
-        return mem[amount]
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for i in range(amount + 1):
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i-c])
+        return dp[amount] if dp[amount] != amount + 1 else -1
