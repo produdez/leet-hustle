86f05d3ec1e80afb500bde6611e017068436e1a2
Produde (Long) <72970246+produdez@users.noreply.github.com>
2024-09-08 18:02:05 +0700

    Time: 1811 ms (27.46%), Space: 16.9 MB (30.96%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,25 +1,10 @@
 class Solution:
-    '''
-        Version: 1
-            DP bottom up
-            -> Cleaned up code
-        Idea:
-            Build the sequences from the right to left
-            each cell in memoize is maxLe of sequence starting with number
-            at that index
-        Complexity:
-        - Time: O(n^2)
-        - Space: O(n)
-        Note:
-            If not for dynamic, complexity would be O(2^n) cause for 
-            each element we have two choice to check
-    '''
     def lengthOfLIS(self, nums: List[int]) -> int:
-        memoize = [1] * len(nums)
-        maxLen = 1
-        for i in reversed(range(len(nums))):
-            for j in range(i + 1, len(nums)):
-                if nums[j] > nums[i]: 
-                    memoize[i] = max(memoize[i], memoize[j] + 1)
-            maxLen = max(maxLen, memoize[i])
-        return maxLen
+        dp = [1] * len(nums)
+        for i in range(0, len(nums) -1)[::-1]:
+            for j in range(i+1, len(nums)):
+                # print(i,j)
+                if nums[i] < nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        # print(dp)
+        return max(dp)
